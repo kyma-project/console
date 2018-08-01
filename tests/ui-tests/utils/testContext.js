ab4352173142d28b6eaa3773922d02bb3946eaee
Kwiatosz, Krzysztof <kwiatekus@users.noreply.github.com>
2018-08-01 08:55:53 +0200

    Check if dex is available before ui test execution

--- tests/ui-tests/utils/testContext.js
+++ tests/ui-tests/utils/testContext.js
@@ -8,12 +8,30 @@ const context = (function() {
     console.log('unhandledRejection', error.message);
   });
 
+  const maxTries = 5;
+  let currentTry = 0;
+
   return {
-    isDexReady: async function(page) {
-      const url = `https://dex.${
-        config.domain
-      }/.well-known/openid-configuration`;
-      return await page.goto(url, { waitUntil: 'networkidle0' });
+    isDexReady: async page => {
+      currentTry++;
+      try {
+        const url = `https://dex.${
+          config.domain
+        }/.well-known/openid-configuration`;
+        return await page.goto(url, { waitUntil: 'networkidle0' });
+      } catch (err) {
+        console.error(
+          `Error while testing dex availibility for the ${currentTry} time`,
+          error
+        );
+        if (currentTry <= maxTries) {
+          setTimeout(() => {
+            return this.isDexReady(page);
+          }, 5000);
+        } else {
+          return false;
+        }
+      }
     },
     getBrowser: () => {
       return puppeteer.launch({
