06b1a2b554eb90fbeb46d6567b2bfee9d8a3b14f
Jasinski, Mariusz <mariusz.jasinski@sap.com>
2018-09-14 11:37:23 +0200

    kinda working small ver of content-ui

--- components/react/webpack.config.js
+++ components/react/webpack.config.js
@@ -8,16 +8,13 @@ let libraryName = pkg.name;
 
 module.exports = {
   entry: ['./src/index'],
-  devtool: 'source-map',
+  // devtool: 'source-map',
   module: {
     loaders: [
       { test: /\.js?$/, loader: 'babel-loader', exclude: /node_modules/ },
       {
         test: /\.css$/,
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
-        ],
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|otf)$/,
@@ -26,8 +23,12 @@ module.exports = {
     ],
   },
   resolve: {
-    modules: [path.resolve('./node_modules'), "node_modules"],
+    modules: [path.resolve('./node_modules'), 'node_modules'],
     extensions: ['.js'],
+    alias: {
+      react: 'preact-compat',
+      'react-dom': 'preact-compat',
+    },
   },
   output: {
     path: path.join(__dirname, '/lib'),
@@ -37,7 +38,8 @@ module.exports = {
     libraryTarget: 'umd',
     umdNamedDefine: true,
   },
-  devtool: 'cheap-eval-source-map',
+  devtool: 'cheap-source-map',
+  // devtool: false,
   devServer: {
     contentBase: './src',
     hot: true,
@@ -46,6 +48,11 @@ module.exports = {
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
   ],
   externals: {
     'styled-components': {
--- content/config-overrides.js
+++ content/config-overrides.js
@@ -1,6 +1,21 @@
 const path = require('path');
+//tricky
+const rewirePreact = require('react-app-rewire-preact');
+const webpack = require('webpack');
 
 module.exports = function override(config, env) {
-  config.resolve.modules = [path.resolve('./node_modules'), 'node_modules']
+  config.resolve.modules = [path.resolve('./node_modules'), 'node_modules'];
+  const languages = ['javascript', 'yaml'].join('|');
+
+  config.plugins.push(
+    new webpack.ContextReplacementPlugin(
+      /highlight\.js\/lib\/languages$/,
+      new RegExp(`^./(\`${languages}\`)$`),
+    ),
+  );
+  config = rewirePreact(config, env);
+
+  // console.log('config', config)
+
   return config;
 };
