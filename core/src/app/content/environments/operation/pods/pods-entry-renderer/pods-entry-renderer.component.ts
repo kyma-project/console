f448772ee3e03d257f983d091c45380046849a3c
Sudczak, Jan <jan.sudczak@sap.com>
2018-10-05 15:13:45 +0200

    Create separate component for status

--- core/src/app/content/environments/operation/pods/pods-entry-renderer/pods-entry-renderer.component.ts
+++ core/src/app/content/environments/operation/pods/pods-entry-renderer/pods-entry-renderer.component.ts
@@ -3,6 +3,7 @@ import { Pod } from '../../../../../shared/datamodel/k8s/pods';
 import { AbstractKubernetesEntryRendererComponent } from '../../abstract-kubernetes-entry-renderer.component';
 import { Subscription } from 'rxjs';
 import { ComponentCommunicationService } from '../../../../../shared/services/component-communication.service';
+import { StatusLabelComponent } from '../../../../../shared/components/status-label';
 
 @Component({
   selector: 'app-pods-entry-renderer',
---
+++ core/src/app/shared/components/status-label/status-label.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-status-label',
+  templateUrl: './status-label.component.html',
+  styleUrls: ['./status-label.component.scss']
+})
+export class StatusLabelComponent {}
