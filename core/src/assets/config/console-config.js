0a0185d51215f966f9050dfe176bd628b015b876
Markus <1720843+maxmarkus@users.noreply.github.com>
2018-11-27 13:19:42 +0100

    Add context switcher (#6)
    * changed environments to dynamic node, moved env workspace to parent environments node, modified env call and added configuration for context switcher
    
    * renamed variable
    
    * removed temporary disableAutoLogin
    
    * removed obsolete caching from config
    
    * config cleanup

--- core/src/assets/config/console-config.js
+++ core/src/assets/config/console-config.js
@@ -21,8 +21,9 @@ if (localStorage.getItem('luigi.auth')) {
   token = JSON.parse(localStorage.getItem('luigi.auth')).idToken;
 }
 
-function getNodes(environment) {
-  var nodes = [
+function getNodes(context) {
+  var environment = context.environmentId;
+  return [
     {
       pathSegment: 'details',
       label: 'Overview',
@@ -144,24 +145,15 @@ function getNodes(environment) {
       viewUrl: '/consoleapp.html#/home/environments/' + environment + '/secrets'
     }
   ];
-
-  return nodes;
 }
 
 function getEnvs() {
   reloginIfTokenExpired();
-
   return new Promise(function(resolve, reject) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() {
       if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
         var envs = [];
-        envs.push({
-          pathSegment: 'workspace',
-          label: 'Workspace',
-          viewUrl: '/consoleapp.html#/home/environments/workspace',
-          hideFromNav: true
-        });
         JSON.parse(xmlHttp.response).items.forEach(env => {
           envName = env.metadata.name;
           envs.push({
@@ -169,11 +161,7 @@ function getEnvs() {
             category: 'Environments',
             navigationContext: 'environments',
             label: envName,
-            pathSegment: envName,
-            context: {
-              environmentId: envName
-            },
-            children: getNodes(envName)
+            pathValue: envName
           });
         });
         resolve(envs);
@@ -208,6 +196,7 @@ function reloginIfTokenExpired() {
     relogin();
   }
 }
+
 Luigi.setConfig({
   auth: {
     use: 'openIdConnect',
@@ -216,8 +205,6 @@ Luigi.setConfig({
       client_id: 'console',
       scope:
         'audience:server:client_id:kyma-client audience:server:client_id:console openid profile email groups',
-      redirect_uri: 'http://console-dev.kyma.local:4200',
-      logoutUrl: 'http://console-dev.kyma.local:4200',
       automaticSilentRenew: true,
       loadUserInfo: false
     },
@@ -243,11 +230,20 @@ Luigi.setConfig({
       {
         pathSegment: 'environments',
         label: 'Overview',
-        defaultPathSegment: 'workspace',
+        viewUrl: '/consoleapp.html#/home/environments/workspace',
         context: {
           idToken: token
         },
-        children: getEnvs
+        children: [
+          {
+            // has to be visible for all views exept 'settings'
+            pathSegment: ':environmentId',
+            context: {
+              environmentId: ':environmentId'
+            },
+            children: getNodes
+          }
+        ]
       },
       {
         pathSegment: 'home',
@@ -310,7 +306,29 @@ Luigi.setConfig({
           }
         ]
       }
-    ]
+    ],
+    contextSwitcher: {
+      defaultLabel: 'Select Environment ...',
+      parentNodePath: '/environments', // absolute path
+      lazyloadOptions: true, // load options on click instead on page load
+      options: getEnvs,
+      actions: [
+        // {
+        //   label: '+ New Environment',
+        //   link: '/create-environment'
+        // }
+      ],
+
+      /**
+       * fallbackLabelResolver
+       * Resolve what do display in the context switcher (Label) in case the activated
+       * context (option) is not listed in available options (eg kyma-system namespace),
+       * or if options have not been fetched yet
+       */
+      fallbackLabelResolver: id => {
+        return id.replace(/\b\w/g, l => l.toUpperCase());
+      }
+    }
   },
   routing: {
     nodeParamPrefix: '~',
