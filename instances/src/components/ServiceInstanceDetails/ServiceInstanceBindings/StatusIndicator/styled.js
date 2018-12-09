c55580dc01ae3f46e4de771a4679b3828e8846f6
Maciej Urbańczyk <maciej.urbanczyk@sap.com>
2018-12-09 10:30:37 +0100

    Improve statusIndicator and check bindingCreateParameterSchema exists

--- instances/src/components/ServiceInstanceDetails/ServiceInstanceBindings/StatusIndicator/styled.js
+++ instances/src/components/ServiceInstanceDetails/ServiceInstanceBindings/StatusIndicator/styled.js
@@ -5,22 +5,23 @@ export const StatusWrapper = styled.div`
     props.backgroundColor ? props.backgroundColor : '#b0b2b4'};
   position: relative;
   border-radius: 50%;
-  width: 16px;
-  height: 16px;
-  margin-left: 10px;
+  width: 14px;
+  height: 14px;
+  margin-left: 5px;
   border: none;
   overflow: hidden;
 `;
+
 export const Status = styled.span`
   position: absolute;
   left: 50%;
   top: 50%;
   border: none;
-  transform: translate(-50%, -50%);
+  transform: translate(-55%, -50%);
   margin: 0;
   padding: 0;
   font-family: '72';
-  line-height: 16px;
-  font-size: 10px;
+  line-height: 14px;
+  font-size: 9px;
   color: #ffffff;
 `;
