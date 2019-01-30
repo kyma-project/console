8a37abc781ed8816cdc18748ebeb461520577d58
Maciej Urbańczyk <maciej.urbanczyk@sap.com>
2019-01-30 16:19:13 +0100

    Improve styles for service-catalog views

--- components/react/src/components/Label/index.js
+++ components/react/src/components/Label/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { LabelWrapper, Label } from './styled';
 
-export default ({ children, cursorType }) => (
+export default ({ children, cursorType }) => { console.log(cursorType); return (
     <LabelWrapper cursorType={cursorType}>
         <Label>{children}</Label>
     </LabelWrapper>
-);
+)};
