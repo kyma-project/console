ae662a0251b1eae088f7e3123f0836f58240959d
Sudczak, Jan <jan.sudczak@sap.com>
2019-01-31 16:58:16 +0100

    Eliminate popover click propagation bug

--- components/angular/generic-list/src/app/modules/list/list-element-actions/list-element-actions.component.ts
+++ components/angular/generic-list/src/app/modules/list/list-element-actions/list-element-actions.component.ts
@@ -1,54 +1,46 @@
-import {ChangeDetectorRef, Component, Input, ViewRef} from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  Input,
+  ViewRef,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
 
 @Component({
   selector: 'y-list-actions',
   templateUrl: './list-element-actions.component.html',
-  styleUrls: ['./list-element-actions.component.scss']
+  styleUrls: ['./list-element-actions.component.scss'],
 })
 export class ListElementActionsComponent {
-
   @Input() entry: any;
   @Input() entryEventHandler: any;
   @Input() actions: any[];
+  @ViewChild('popover') popover: ElementRef;
 
   isExpanded = false;
   ariaHidden = true;
 
   constructor(private changeDetector: ChangeDetectorRef) {}
+  //---------------------------------
 
-  toggleActionsBtn(event) {
+  handlePopoverClick(event) {
     event.stopPropagation();
-    this.isExpanded = !this.isExpanded;
-    this.ariaHidden = !this.ariaHidden;
-      if (!(this.changeDetector as ViewRef).destroyed) {
-          this.changeDetector.detectChanges();
-      }
-  }
-
-  closeActionsBtn(event) {
-    event.stopPropagation();
-    this.isExpanded = false;
-    this.ariaHidden = true;
-      if (!(this.changeDetector as ViewRef).destroyed) {
-          this.changeDetector.detectChanges();
-      }
+    this.popover.onClickHandler(event);
   }
 
   executeAction(action: string, event) {
     event.stopPropagation();
     const functionName = action['function'];
-    if (functionName && this.entryEventHandler && this.entryEventHandler[functionName]) {
+    if (
+      functionName &&
+      this.entryEventHandler &&
+      this.entryEventHandler[functionName]
+    ) {
       this.entryEventHandler[functionName](this.entry);
-        if (!(this.changeDetector as ViewRef).destroyed) {
-            this.changeDetector.detectChanges();
-        }
+      if (!(this.changeDetector as ViewRef).destroyed) {
+        this.changeDetector.detectChanges();
+      }
     }
-  }
-
-  entryAsString() {
-    if (this.entry instanceof Object) {
-      return this.entry.toString();
-    }
-    return this.entry;
   }
 }
