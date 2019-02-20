195fbdbafb9ce3dcd5c61e43758d477cd5c01f9e
Mateusz Puczynski <mateusz.puczynski@sap.com>
2019-02-20 11:17:49 +0100

    add mocks to ./ODataFiles/index.ts

--- components/react-odata/src/ODataReact.tsx
+++ components/react-odata/src/ODataReact.tsx
@@ -1,27 +1,13 @@
 import React from "react";
 import { parse } from "./tools/Parser";
 import { Child } from "./types";
-// import ODataFile from './ODataFiles/ODataProductV2'; //does not work, properties cut off
-// import ODataFile from './ODataFiles/ODataNext'; //library cuts off many fields, not working
-// import ODataFile from './ODataFiles/ODataProductV2'; //doesnt work
-// import ODataFile from './ODataFiles/ODataFile'; //works
-// import ODataFile from './ODataFiles/SAPodata'; //works
-// import ODataFile from './ODataFiles/ODataTripV4'; //works
-// import ODataFile from './ODataFiles/ODataFavourite3'; //works - no annotations
-// import ODataFile from './ODataFiles/ODataFav5'; //works
-// import ODataFile from './ODataFiles/ODataFav1'; //works - basic annotations
-// import ODataFile from './ODataFiles/ODataProductV4'; //works - use this to see Service Documentation / Annotations
-// import ODataFile from './ODataFiles/ODataProductsV3'; //works
-// import ODataFile from "./ODataFiles/ODataFav11"; // works //most complex
-import ODataFile from "./ODataFiles/ODataFav21"; // works // there are errors in console
-// import ODataFile from "./ODataFiles/ODataNorthWindV2"; // works
-// import ODataFile from "./ODataFiles/ODataFav3"; // works, but there are certain errors - look at console
+import { mocks } from "./ODataFiles/index";
 
 import TableContainer from "./components/Table/TableContainer";
 import { ErrorComponent } from "./components/ErrorComponent/ErrorComponent";
 
 const ODataReact = () => {
-  const data = parse.ParseFromString(ODataFile);
+  const data = parse.ParseFromString(mocks.ODataFav11);
   const schema = data.getElementsByTagName("Schema");
 
   if (schema.length < 1) {
