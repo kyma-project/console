d367baa5c76244da0ea4bd8dc113f849e88328e7
Markus <1720843+maxmarkus@users.noreply.github.com>
2019-03-08 10:47:08 +0100

    563 apply fiori fundamentals phase 2 (part 1) (#65)

--- tests/ui-tests/commands/console.js
+++ tests/ui-tests/commands/console.js
@@ -116,7 +116,7 @@ async function getEnvironmentsFromContextSwitcher(page) {
 }
 
 async function getEnvironmentNamesFromEnvironmentsPage(page) {
-  return await getNamesOnCurrentPage(page, '.tn-card__header');
+  return await getNamesOnCurrentPage(page, '[data-e2e-id=namespace-name]');
 }
 
 async function getRemoteEnvironmentNames(page) {
