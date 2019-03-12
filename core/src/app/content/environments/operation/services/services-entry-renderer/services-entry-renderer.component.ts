b5bd5516d8d55ef7a9aeeb2d1ad9862feefa2b54
dariadomagala <daria.domagala@sap.com>
2019-03-12 11:55:28 +0100

    Hide 'expose api' action for system namespaces

--- core/src/app/content/environments/operation/services/services-entry-renderer/services-entry-renderer.component.ts
+++ core/src/app/content/environments/operation/services/services-entry-renderer/services-entry-renderer.component.ts
@@ -17,22 +17,37 @@ export class ServicesEntryRendererComponent
     private componentCommunicationService: ComponentCommunicationService
   ) {
     super(injector);
+    this.contextListenerId = LuigiClient.addContextUpdateListener(context => {
+      if (context) {
+        this.isSystemNamespace = context.isSystemNamespace;
+      }
+    });
   }
+  contextListenerId: number;
+  isSystemNamespace: boolean;
   public disabled = false;
   private communicationServiceSubscription: Subscription;
 
   ngOnInit() {
-    this.actions = [
-      {
-        function: 'exposeApi',
-        name: 'Expose API'
-      },
-      {
-        function: 'details',
-        name: 'Details'
-      },
-      ...this.actions
-    ];
+      this.actions = this.isSystemNamespace ? 
+      [
+        {
+          function: 'details',
+          name: 'Details'
+        },
+        ...this.actions
+      ] : 
+      [
+        {
+          function: 'exposeApi',
+          name: 'Expose API'
+        },
+        {
+          function: 'details',
+          name: 'Details'
+        },
+        ...this.actions
+      ];
     this.communicationServiceSubscription = this.componentCommunicationService.observable$.subscribe(
       e => {
         const event: any = e;
@@ -48,6 +63,7 @@ export class ServicesEntryRendererComponent
 
   ngOnDestroy() {
     this.communicationServiceSubscription.unsubscribe();
+    LuigiClient.removeContextUpdateListener(this.contextListenerId);
   }
 
   isStatusOk(entry) {
