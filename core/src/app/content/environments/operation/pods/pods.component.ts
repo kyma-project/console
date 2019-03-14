a2081d6a50cb32bfface3b788a624c821daf5908
dariadomagala <daria.domagala@sap.com>
2019-03-14 09:49:04 +0100

    Use graphql for editing and deleting

--- core/src/app/content/environments/operation/pods/pods.component.ts
+++ core/src/app/content/environments/operation/pods/pods.component.ts
@@ -1,49 +1,40 @@
 import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, Subscription } from 'rxjs';
-
 import { CurrentEnvironmentService } from 'environments/services/current-environment.service';
-import { AppConfig } from '../../../../app.config';
-import { AbstractKubernetesElementListComponent } from '../abstract-kubernetes-element-list.component';
-import { PodsHeaderRendererComponent } from './pods-header-renderer/pods-header-renderer.component';
-import { KubernetesDataProvider } from '../kubernetes-data-provider';
-import { PodsEntryRendererComponent } from './pods-entry-renderer/pods-entry-renderer.component';
 import { ComponentCommunicationService } from 'shared/services/component-communication.service';
-import { DataConverter } from 'app/generic-list';
-import { IPod, Pod } from 'shared/datamodel/k8s/pod';
 import { GraphQLClientService } from 'shared/services/graphql-client-service';
-import { GraphQLDataProvider } from '../graphql-data-provider';
+import { AbstractGraphqlElementListComponent } from '../abstract-graphql-element-list.component';
+import { PodsEntryRendererComponent } from './pods-entry-renderer/pods-entry-renderer.component';
+import { PodsHeaderRendererComponent } from './pods-header-renderer/pods-header-renderer.component';
 
 @Component({
   templateUrl: '../kubernetes-element-list.component.html'
 })
-export class PodsComponent extends AbstractKubernetesElementListComponent
+export class PodsComponent extends AbstractGraphqlElementListComponent
   implements OnDestroy {
-  public title = 'Pods';
-  public emptyListText =
+  title = 'Pods';
+  emptyListText =
     'It looks like you don’t have any pods in your namespace yet.';
-  public createNewElementText = 'Add Pod';
-  public resourceKind = 'Pod';
-  private currentEnvironmentId: string;
-  private currentEnvironmentSubscription: Subscription;
-  public hideFilter = false;
-  public baseUrl: string;
+  resourceKind = 'Pod';
+
+  entryRenderer = PodsEntryRendererComponent;
+  headerRenderer = PodsHeaderRendererComponent;
 
   constructor(
-    private http: HttpClient,
-    private currentEnvironmentService: CurrentEnvironmentService,
-    private commService: ComponentCommunicationService,
-    private graphQLClientService: GraphQLClientService,
+    currentEnvironmentService: CurrentEnvironmentService,
+    commService: ComponentCommunicationService,
+    graphQLClientService: GraphQLClientService,
     changeDetector: ChangeDetectorRef
   ) {
-    super(currentEnvironmentService, changeDetector, http, commService);
-    const converter: DataConverter<IPod, Pod> = {
-      convert(entry: IPod) {
-        return new Pod(entry);
-      }
-    };
+    super(
+      currentEnvironmentService, 
+      commService, 
+      graphQLClientService, 
+      changeDetector
+    );
+  }
 
-    const query = `query Pod($namespace: String!) {
+  getGraphglQueryForList() {
+    return `query Pod($namespace: String!) {
       pods(namespace: $namespace) {
         name
         nodeName
@@ -60,38 +51,6 @@ export class PodsComponent extends AbstractKubernetesElementListComponent
         json
       }
     }`;
-
-    this.currentEnvironmentSubscription = this.currentEnvironmentService
-      .getCurrentEnvironmentId()
-      .subscribe(envId => {
-        this.currentEnvironmentId = envId;
-        this.baseUrl = `${AppConfig.k8sApiServerUrl}namespaces/${
-          this.currentEnvironmentId
-        }/pods`;
-
-        this.source = new GraphQLDataProvider(
-          AppConfig.graphqlApiUrl, 
-          query, 
-          {
-            namespace: this.currentEnvironmentId,
-          },
-          this.graphQLClientService
-          )
-
-        this.entryRenderer = PodsEntryRendererComponent;
-        this.headerRenderer = PodsHeaderRendererComponent;
-      });
   }
-
-  public getResourceUrl(kind: string, entry: any): string {
-    return `${this.baseUrl}/${entry.name}`;
-  }
-
-  public createNewElement() {
-    // TODO
-  }
-
-  public ngOnDestroy() {
-    this.currentEnvironmentSubscription.unsubscribe();
-  }
+  
 }
