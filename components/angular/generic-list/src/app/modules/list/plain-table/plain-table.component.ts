dc02cf394ba9107d91d3b8fafa09b6f8c7128c09
Jesus Real <8238188+jesusreal@users.noreply.github.com>
2019-03-19 16:16:54 +0100

    Pass through showEmptyPage boolean to be applied on empty list

--- components/angular/generic-list/src/app/modules/list/plain-table/plain-table.component.ts
+++ components/angular/generic-list/src/app/modules/list/plain-table/plain-table.component.ts
@@ -20,6 +20,7 @@ export class PlainTableComponent extends PlainListComponent {
   @Input() headerRenderer: Type<any>;
   @Input() footerRenderer: Type<any>;
   @Input() entryTagName = 'tbody';
+  @Input() showEmptyPage: Boolean;
   @Input()
   emptyListPlaceholderData: {
     header: { text: string; actionButton: { glyph: string; text: string } };
