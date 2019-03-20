3c98c7b4ffbb064410bcb6c678b504ed0b03bd1d
dariadomagala <daria.domagala@sap.com>
2019-03-20 10:32:30 +0100

    Clear binding modal data after closing it

--- core/src/app/shared/components/role-binding-modal/role-binding-modal.component.ts
+++ core/src/app/shared/components/role-binding-modal/role-binding-modal.component.ts
@@ -107,16 +107,22 @@ export class RoleBindingModalComponent implements OnDestroy {
     }
     this.modalService.open(this.createBindingModal).result.finally(() => {
       this.isActive = false;
+      this.clearData();
     });
     this.isUserGroupMode = true;
   }
 
   public close() {
+    this.clearData();
     this.modalService.close(this.createBindingModal);
+  }
+
+  public clearData() {
     this.userOrGroup = '';
     this.selectedRole = '';
     this.selectedKind = '';
     this.error = '';
+    this.userGroupError = '';
   }
 
   prepareData() {
