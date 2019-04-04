7f79e1e263733df04cf76f7d10f809cd7306a51d
Agata Kucharska <agata.kucharska@sap.com>
2019-04-04 16:18:50 +0200

    Add notes and code copy button

--- components/react/src/components/ReactMarkdown/components/Code/CopyButton.js
+++ components/react/src/components/ReactMarkdown/components/Code/CopyButton.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
-// import React, { useContext } from "react";
 import styled from 'styled-components';
-
 import { CopyToClipboard } from 'react-copy-to-clipboard';
-
-// import PopupState from "@common/state/usePopup";
 
 import Icon from '../../../Icon';
 import Tooltip from '../../../Tooltip';
@@ -36,9 +32,22 @@ const ConfirmationWrapper = styled.div`
   bottom: 30px;
   left: 50%;
   transform: translateX(-50%);
+  box-sizing: border-box;
+  z-index: 99;
+  min-width: 120px;
+  max-width: 420px;
+  background: #32363a;
+  font-size: 12px;
+  line-height: 12px;
+  color: #fff;
+  filter: drop-shadow(rgba(0, 0, 0, 0.12) 0 0px 2px);
+  box-shadow: 0 0 4px 0 #00000026, 0 12px 20px 0 #00000019;
+  border-radius: 3px;
+  padding: 6px 10px;
+  font-weight: bold;
 `;
 
-const CONFIRMATION_VISIBILITY_TIME = 1500;
+const CONFIRMATION_VISIBILITY_TIME = 2000;
 
 class CopyButton extends Component {
   constructor(props) {
@@ -52,23 +61,32 @@ class CopyButton extends Component {
 
   toggleConfirmation = confirmation => {
     this.setState({
-      showConfirmation: !confirmation,
+      showConfirmation: confirmation,
     });
   };
+
+  scheduleHideConfirmation() {
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => {
+      this.toggleConfirmation(false);
+    }, CONFIRMATION_VISIBILITY_TIME);
+  }
 
   render() {
     const { code, className = '' } = this.props;
     const showConfirmation = this.state.showConfirmation;
     const tooltipDescription = 'Click to copy';
     const copyPopupDescription = 'Copied to clipboard';
+    if (showConfirmation) {
+      this.scheduleHideConfirmation();
+    }
     return (
       <CopyButtonWrapper className={className}>
         <Tooltip content={tooltipDescription} orientation={'bottom'}>
           <CopyToClipboard
             text={code}
             onCopy={() => {
-              alert(copyPopupDescription);
-              // setPopup(copyPopupDescription);
+              this.toggleConfirmation(true);
             }}
           >
             <span>
