a05d29c2db5d9d887386421876a19fa237ac82dc
Mateusz Puczynski <mateusz.puczynski@sap.com>
2019-04-10 15:59:22 +0200

    update test file to check something

--- tests/ui-tests/utils/testContext.js
+++ tests/ui-tests/utils/testContext.js
@@ -14,7 +14,7 @@ const context = (function() {
       return puppeteer.launch({
         ignoreHTTPSErrors: true,
         headless: config.headless,
-        slowMo: 80,
+        slowMo: 100,
         args: [
           `--window-size=${config.viewportWidth},${config.viewportHeight}`,
           '--no-sandbox',
