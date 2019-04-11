8a08e12da5b082fe15f16a98ec37c34bc3bb33a9
Mateusz Puczynski <mateusz.puczynski@sap.com>
2019-04-11 08:45:11 +0200

    revert chenges to few files

--- instances/src/components/ServiceInstances/ServiceInstancesTable/ServiceInstancesTable.component.js
+++ instances/src/components/ServiceInstances/ServiceInstancesTable/ServiceInstancesTable.component.js
@@ -69,140 +69,142 @@ export class ServiceInstancesTable extends Component {
     );
 
     const createTableData = () => {
-      return data.map(instance => ({
-        rowData: [
-          <TextOverflowWrapper>
-            <LinkButton data-e2e-id="instance-name">
-              <Link
-                onClick={() => this.goToServiceInstanceDetails(instance.name)}
-                data-e2e-id={`instance-name-${instance.name}`}
-                title={instance.name}
-              >
-                {instance.name}
-              </Link>
-            </LinkButton>
-          </TextOverflowWrapper>,
-          (_ => {
-            const instanceClass =
-              instance.clusterServiceClass || instance.serviceClass;
-            if (!instanceClass || !instanceClass.name) {
-              return '-';
-            }
-
-            const classTitle = getResourceDisplayName(instanceClass);
-            return (
-              <TextOverflowWrapper>
-                <ServiceClassButton
-                  onClick={() =>
-                    this.goToServiceClassDetails(instanceClass.name)
-                  }
-                  title={classTitle}
+      return data.map(instance => {
+        return {
+          rowData: [
+            <TextOverflowWrapper>
+              <LinkButton data-e2e-id="instance-name">
+                <Link
+                  onClick={() => this.goToServiceInstanceDetails(instance.name)}
+                  data-e2e-id={`instance-name-${instance.name}`}
+                  title={instance.name}
                 >
-                  {classTitle}
-                </ServiceClassButton>
-              </TextOverflowWrapper>
-            );
-          })(),
-          (_ => {
-            const plan = instance.clusterServicePlan || instance.servicePlan;
-            if (!plan) {
-              return '-';
-            }
+                  {instance.name}
+                </Link>
+              </LinkButton>
+            </TextOverflowWrapper>,
+            (_ => {
+              const instanceClass =
+                instance.clusterServiceClass || instance.serviceClass;
+              if (!instanceClass || !instanceClass.name) {
+                return '-';
+              }
 
-            const planDisplayName = getResourceDisplayName(plan);
-
-            if (
-              instance.planSpec &&
-              instance.planSpec !== null &&
-              typeof instance.planSpec === 'object' &&
-              Object.keys(instance.planSpec).length
-            ) {
+              const classTitle = getResourceDisplayName(instanceClass);
               return (
                 <TextOverflowWrapper>
-                  <Modal
-                    title="Instance's Parameters"
-                    modalOpeningComponent={
-                      <ServicePlanButton data-e2e-id="service-plan">
-                        {planDisplayName}
-                      </ServicePlanButton>
+                  <ServiceClassButton
+                    onClick={() =>
+                      this.goToServiceClassDetails(instanceClass.name)
                     }
-                    onShow={() => LuigiClient.uxManager().addBackdrop()}
-                    onHide={() => LuigiClient.uxManager().removeBackdrop()}
+                    title={classTitle}
                   >
-                    <JSONCode data-e2e-id="service-plan-content">
-                      {JSON.stringify(instance.planSpec, null, 2)}
-                    </JSONCode>
-                  </Modal>
+                    {classTitle}
+                  </ServiceClassButton>
                 </TextOverflowWrapper>
               );
-            }
-            return (
-              <TextOverflowWrapper>
-                <span>{planDisplayName}</span>
-              </TextOverflowWrapper>
-            );
-          })(),
-          (_ => {
-            const bindingUsages = this.displayBindingsUsages(
-              instance.serviceBindingUsages,
-            );
+            })(),
+            (_ => {
+              const plan = instance.clusterServicePlan || instance.servicePlan;
+              if (!plan) {
+                return '-';
+              }
 
-            return (
-              <TextOverflowWrapper>
-                <span>{bindingUsages}</span>
-              </TextOverflowWrapper>
-            );
-          })(),
-          (_ => {
-            if (!instance.status) {
-              return '-';
-            }
+              const planDisplayName = getResourceDisplayName(plan);
 
-            let type = '';
-            switch (instance.status.type) {
-              case 'RUNNING':
-                type = 'success';
-                break;
-              case 'FAILED':
-                type = 'error';
-                break;
-              default:
-                type = 'warning';
-            }
-            return (
-              <Tooltip
-                wrapperStyles="max-width: 100%;"
-                type={type}
-                content={instance.status.message}
-                minWidth="250px"
-              >
-                <span
-                  style={{
-                    color: statusColor(instance.status.type),
-                    cursor: 'help',
-                  }}
+              if (
+                instance.planSpec &&
+                instance.planSpec !== null &&
+                typeof instance.planSpec === 'object' &&
+                Object.keys(instance.planSpec).length
+              ) {
+                return (
+                  <TextOverflowWrapper>
+                    <Modal
+                      title="Instance's Parameters"
+                      modalOpeningComponent={
+                        <ServicePlanButton data-e2e-id="service-plan">
+                          {planDisplayName}
+                        </ServicePlanButton>
+                      }
+                      onShow={() => LuigiClient.uxManager().addBackdrop()}
+                      onHide={() => LuigiClient.uxManager().removeBackdrop()}
+                    >
+                      <JSONCode data-e2e-id="service-plan-content">
+                        {JSON.stringify(instance.planSpec, null, 2)}
+                      </JSONCode>
+                    </Modal>
+                  </TextOverflowWrapper>
+                );
+              }
+              return (
+                <TextOverflowWrapper>
+                  <span>{planDisplayName}</span>
+                </TextOverflowWrapper>
+              );
+            })(),
+            (_ => {
+              const bindingUsages = this.displayBindingsUsages(
+                instance.serviceBindingUsages,
+              );
+
+              return (
+                <TextOverflowWrapper>
+                  <span>{bindingUsages}</span>
+                </TextOverflowWrapper>
+              );
+            })(),
+            (_ => {
+              if (!instance.status) {
+                return '-';
+              }
+
+              let type = '';
+              switch (instance.status.type) {
+                case 'RUNNING':
+                  type = 'success';
+                  break;
+                case 'FAILED':
+                  type = 'error';
+                  break;
+                default:
+                  type = 'warning';
+              }
+              return (
+                <Tooltip
+                  wrapperStyles="max-width: 100%;"
+                  type={type}
+                  content={instance.status.message}
+                  minWidth="250px"
                 >
-                  {instance.status.type}
-                </span>
-              </Tooltip>
-            );
-          })(),
-          <Modal
-            title="Warning"
-            content={`Are you sure you want to delete instance "${
-              instance.name
-            }"?`}
-            confirmText="Delete"
-            onConfirm={() => handleDelete(instance)}
-            modalOpeningComponent={deleteButton}
-            type="negative"
-            onShow={() => LuigiClient.uxManager().addBackdrop()}
-            onHide={() => LuigiClient.uxManager().removeBackdrop()}
-          >
-            {`Are you sure you want to delete instance "${instance.name}"?`}
-          </Modal>,
-        ],
-      }));
+                  <span
+                    style={{
+                      color: statusColor(instance.status.type),
+                      cursor: 'help',
+                    }}
+                  >
+                    {instance.status.type}
+                  </span>
+                </Tooltip>
+              );
+            })(),
+            <Modal
+              title="Warning"
+              content={`Are you sure you want to delete instance "${
+                instance.name
+              }"?`}
+              confirmText="Delete"
+              onConfirm={() => handleDelete(instance)}
+              modalOpeningComponent={deleteButton}
+              type="negative"
+              onShow={() => LuigiClient.uxManager().addBackdrop()}
+              onHide={() => LuigiClient.uxManager().removeBackdrop()}
+            >
+              {`Are you sure you want to delete instance "${instance.name}"?`}
+            </Modal>,
+          ],
+        };
+      });
     };
 
     const addServiceInstanceRedirectButton = (
--- tests/ui-tests/utils/testContext.js
+++ tests/ui-tests/utils/testContext.js
@@ -14,7 +14,7 @@ const context = (function() {
       return puppeteer.launch({
         ignoreHTTPSErrors: true,
         headless: config.headless,
-        slowMo: 100,
+        slowMo: 80,
         args: [
           `--window-size=${config.viewportWidth},${config.viewportHeight}`,
           '--no-sandbox',
