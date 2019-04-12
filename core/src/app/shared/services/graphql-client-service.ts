fb229dca5807cc77e65c80d0966957a7a4061bd7
dariadomagala <daria.domagala@sap.com>
2019-04-12 15:06:03 +0200

    Use apollo for all views

--- core/src/app/shared/services/graphql-client-service.ts
+++ core/src/app/shared/services/graphql-client-service.ts
@@ -1,39 +1,23 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { concatMap, catchError } from 'rxjs/operators';
-import { Observable, of, throwError } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
 
 @Injectable()
 export class GraphQLClientService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private apollo: Apollo
+  ) {}
 
-  request(url, query, variables): Observable<any> {
-    return this.http.post(url, { query, variables }).pipe(
-      concatMap(res => {
-        const response: any = res;
-        const filteredErrors =
-          (response &&
-            response.errors &&
-            response.errors.filter(
-              (e: any) => !e.message.startsWith('MODULE_DISABLED')
-            )) ||
-          [];
-        if (filteredErrors.length) {
-          return throwError(filteredErrors[0].message);
-        } else if (response && response.data) {
-          return of(response.data);
-        }
-      }),
-      catchError(err => {
-        let error;
-        if (err.error && err.error.errors) {
-          error = err.error.errors[0].message;
-        } else if (err.error && err.error.message) {
-          error = err.error.message;
-        }
-        error = error || err.message || err;
-        return throwError(error);
-      })
-    );
+  gqlQuery (query, variables = {}) {
+    return this.apollo
+    .watchQuery({query: gql`${query}`, variables}).valueChanges
+    .pipe(map(res => res.data));
+  }
+
+  gqlMutation (query, variables = {}) {
+    return this.apollo
+    .mutate({mutation: gql`${query}`, variables})
+    .pipe(map(res => res.data));
   }
 }
