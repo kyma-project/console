822913ee9c4cb5d231d75890816ed6000ae64e7e
dariadomagala <daria.domagala@sap.com>
2019-04-15 11:46:46 +0200

    Remove old workaround

--- core/src/app/ws.ts
+++ core/src/app/ws.ts
@@ -1,5 +1,5 @@
-import { ApolloLink, FetchResult } from 'apollo-link';
-import { SubscriptionClient, Operation } from 'subscriptions-transport-ws';
+import { ApolloLink } from 'apollo-link';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
 import LuigiClient from '@kyma-project/luigi-client';
 
 export class WebSocketLink extends ApolloLink {
@@ -7,36 +7,18 @@ export class WebSocketLink extends ApolloLink {
   constructor(paramsOrClient) {
     super();
 
+    const token = LuigiClient.getEventData().idToken;
+    const protocols = ['graphql-ws', token];
+
     if (paramsOrClient instanceof SubscriptionClient) {
       this.subscriptionClient = paramsOrClient;
     } else {
       this.subscriptionClient = new SubscriptionClient(
         paramsOrClient.uri,
         paramsOrClient.options,
-        getWrappedWebsocket(),
+        null,
+        protocols
       );
     }
   }
-
-  request(operation: Operation) {
-    return this.subscriptionClient.request(operation);
-  }
-}
-
-/**
- * TODO: Remove this function once the PR is merged
- * https://github.com/apollographql/subscriptions-transport-ws/pull/477
- */
-function getWrappedWebsocket() {
-  const NativeWebSocket = window.WebSocket || window.MozWebSocket;
-  const customWs = url => {
-    const token = LuigiClient.getEventData().idToken;
-    const protocols = ['graphql-ws', token];
-    return new NativeWebSocket(url, protocols);
-  };
-  customWs.OPEN = NativeWebSocket.OPEN;
-  customWs.CONNECTING = NativeWebSocket.CONNECTING;
-  customWs.CLOSED = NativeWebSocket.CLOSED;
-
-  return customWs;
 }
