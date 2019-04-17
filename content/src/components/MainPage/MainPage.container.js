8ed30f824d356541eeb990eca5bf7d2f9b234b25
Mateusz Puczynski <mateusz.puczynski@sap.com>
2019-04-17 09:56:04 +0200

    update query

--- content/src/components/MainPage/MainPage.container.js
+++ content/src/components/MainPage/MainPage.container.js
@@ -8,43 +8,52 @@ import { CLUSTER_DOCS_TOPICS } from './queries';
 const filterExtensions = ['md'];
 
 export default compose(
+  // graphql(CLUSTER_DOCS_TOPICS, {
+  //   name: 'clusterDocsTopicsComponents',
+  //   options: {
+  //     variables: {
+  //       viewContext: 'docs-ui',
+  //       groupName: 'components',
+  //       filterExtensions: filterExtensions,
+  //     },
+  //   },
+  // }),
+  // graphql(CLUSTER_DOCS_TOPICS, {
+  //   name: 'clusterDocsTopicsRoot',
+  //   options: {
+  //     variables: {
+  //       viewContext: 'docs-ui',
+  //       groupName: 'root',
+  //       filterExtensions: filterExtensions,
+  //     },
+  //   },
+  // }),
   graphql(CLUSTER_DOCS_TOPICS, {
-    name: 'clusterDocsTopicsComponents',
     options: {
       variables: {
-        viewContext: 'docs-ui',
-        groupName: 'components',
-        filterExtensions: filterExtensions,
-      },
-    },
-  }),
-  graphql(CLUSTER_DOCS_TOPICS, {
-    name: 'clusterDocsTopicsRoot',
-    options: {
-      variables: {
-        viewContext: 'docs-ui',
-        groupName: 'root',
         filterExtensions: filterExtensions,
       },
     },
   }),
 )(props => {
-  if (
-    props.clusterDocsTopicsRoot.loading ||
-    props.clusterDocsTopicsComponents.loading ||
-    !props.clusterDocsTopicsRoot.clusterDocsTopics ||
-    !props.clusterDocsTopicsComponents.clusterDocsTopics
-  ) {
+  const {
+    data: { loading, clusterDocsTopics },
+  } = props;
+
+  if (loading || !clusterDocsTopics) {
     return null;
   }
 
+  // return <section>{JSON.stringify(props)}</section>;
   return (
     <MainPage
       {...props}
-      clusterDocsTopicsRoot={props.clusterDocsTopicsRoot.clusterDocsTopics}
-      clusterDocsTopicsComponents={
-        props.clusterDocsTopicsComponents.clusterDocsTopics
-      }
+      clusterDocsTopicsRoot={clusterDocsTopics.find(
+        elem => elem.name === 'kyma',
+      )}
+      clusterDocsTopicsComponents={clusterDocsTopics.filter(
+        elem => elem.name !== 'kyma',
+      )}
     />
   );
 });
