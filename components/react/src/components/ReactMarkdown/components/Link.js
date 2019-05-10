ccd6b30f61dd7d47beed3fa60be2f2799ce672d0
Mateusz Puczynski <mateusz.puczynski@sap.com>
2019-05-10 10:05:13 +0200

    add `rel="noopener noreferrer"`

--- components/react/src/components/ReactMarkdown/components/Link.js
+++ components/react/src/components/ReactMarkdown/components/Link.js
@@ -4,6 +4,6 @@ import styled from 'styled-components';
 const StyledLink = styled.a``;
 
 export const Link = props => {
-  const { target, ...rest } = props;
-  return <StyledLink target="_blank" {...rest} />;
+  const { target, rel, ...rest } = props;
+  return <StyledLink target="_blank" rel="noopener noreferrer" {...rest} />;
 };
