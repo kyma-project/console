cde49c98c89a0a7adaab0657741fb535dcf0dd1e
parostatkiem <jan.sudczak@sap.com>
2019-05-28 11:33:53 +0200

    Add help tooltip to 'port' column

--- core/src/app/content/namespaces/configuration/apis/api-definition-entry-renderer/api-definition-entry-renderer.component.ts
+++ core/src/app/content/namespaces/configuration/apis/api-definition-entry-renderer/api-definition-entry-renderer.component.ts
@@ -5,6 +5,7 @@ import * as _ from 'lodash';
 import { ComponentCommunicationService } from '../../../../../shared/services/component-communication.service';
 import { Subscription } from 'rxjs';
 import LuigiClient from '@kyma-project/luigi-client';
+import { API_PORT_DESCRIPTION } from '../../../../../shared/constants/constants';
 
 @Component({
   selector: 'app-api-definition-entry-renderer',
@@ -15,6 +16,7 @@ export class ApiDefinitionEntryRendererComponent
   implements OnDestroy, OnInit {
   public currentNamespaceId: string;
   private currentNamespaceSubscription: Subscription;
+  readonly API_PORT_DESCRIPTION = API_PORT_DESCRIPTION;
 
   constructor(
     protected injector: Injector,
