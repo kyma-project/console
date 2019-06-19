cfa44a5c42a8c570081ec09bd8989a9a1dbe7f61
parostatkiem <jan.sudczak@sap.com>
2019-06-19 13:17:35 +0200

    Add missing reload() to namespace subscription

--- core/src/app/content/namespaces/operation/abstract-graphql-element-list.component.ts
+++ core/src/app/content/namespaces/operation/abstract-graphql-element-list.component.ts
@@ -69,6 +69,7 @@ export class AbstractGraphqlElementListComponent
           this.getGraphqlSubscriptionsForList(),
           this.resourceKind
         );
+        this.reload();
       });
   }
 
