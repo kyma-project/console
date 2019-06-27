e5a8d7d43155845eaca529b2deef64d0af951a54
Wawrzyn321 <Wawrzyn321@gmail.com>
2019-06-27 09:45:34 +0200

    Moved title

--- instances/src/components/ServiceInstanceDetails/ServiceInstanceToolbar/ServiceInstanceToolbar.component.js
+++ instances/src/components/ServiceInstanceDetails/ServiceInstanceToolbar/ServiceInstanceToolbar.component.js
@@ -3,17 +3,15 @@ import LuigiClient from '@kyma-project/luigi-client';
 import ServiceInstanceUpperBar from './../ServiceInstanceUpperBar/ServiceInstanceUpperBar.component';
 
 import {
-  Button,
-  Modal,
   Toolbar,
 } from '@kyma-project/react-components';
 
-import {
-  ServiceInstanceToolbarHeadline,
-  ServiceInstanceToolbarHeadlineLink,
-  UpperBarWrapper
-} from './styled';
-import { styled } from '@kyma-project/asyncapi-react/lib/theme';
+// import {
+//   ServiceInstanceToolbarHeadline,
+//   ServiceInstanceToolbarHeadlineLink,
+//   UpperBarWrapper
+// } from './styled';
+// import { styled } from '@kyma-project/asyncapi-react/lib/theme';
 
 const ServiceInstanceToolbar = ({
   serviceInstance,
@@ -21,26 +19,20 @@ const ServiceInstanceToolbar = ({
   history,
 }) => {
 
-  const goToServiceInstances = () => {
-    LuigiClient.linkManager()
-      .fromContext('namespaces')
-      .navigate('cmf-instances');
-  };
-
   const instanceClass =
     serviceInstance &&
     (serviceInstance.clusterServiceClass
       ? serviceInstance.clusterServiceClass
       : serviceInstance.serviceClass);
 
-  const toolbarTitle = (
-    <ServiceInstanceToolbarHeadline>
-      <ServiceInstanceToolbarHeadlineLink onClick={goToServiceInstances}>
-        Service Instances
-      </ServiceInstanceToolbarHeadlineLink>{' '}
-      / {serviceInstance.name}
-    </ServiceInstanceToolbarHeadline>
-  );
+  // const toolbarTitle = (
+  //   <ServiceInstanceToolbarHeadline>
+  //     <ServiceInstanceToolbarHeadlineLink onClick={goToServiceInstances}>
+  //       Service Instances
+  //     </ServiceInstanceToolbarHeadlineLink>{' '}
+  //     / {serviceInstance.name}
+  //   </ServiceInstanceToolbarHeadline>
+  // );
 
   return (
     <div style={{ backgroundColor: 'white' }}>
@@ -49,7 +41,7 @@ const ServiceInstanceToolbar = ({
           deleteServiceInstance={deleteServiceInstance}
           history={history}/>
       <Toolbar
-        title={toolbarTitle}
+        title={serviceInstance.name}
         description={instanceClass && instanceClass.description}
       />
     </div>
--- instances/src/components/ServiceInstanceDetails/ServiceInstanceUpperBar/styled.js
+++ instances/src/components/ServiceInstanceDetails/ServiceInstanceUpperBar/styled.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const UpperBarWrapper = styled.div`{
   display: flex;
   justify-content: space-between;
+  max-height: 1.25em;
   & > * {
     margin: 32px 32px;
   }
