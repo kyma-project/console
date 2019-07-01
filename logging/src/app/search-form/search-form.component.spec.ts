72d78e5ec67428bad7be6e4ff6ccdb0983d7d26d
parostatkiem <jan.sudczak@sap.com>
2019-07-01 15:11:13 +0200

    fix unit tests

--- logging/src/app/search-form/search-form.component.spec.ts
+++ logging/src/app/search-form/search-form.component.spec.ts
@@ -10,6 +10,7 @@ import { of } from 'rxjs';
 import { ISearchFormData } from './data';
 import { PodsSubscriptonService } from './service/pods-subscription/pods-subscription.service';
 import { Apollo } from 'apollo-angular';
+import AppAriaDisabled from './shared/appAriaDisabled.directive';
 
 const ActivatedRouteMock = {
   queryParams: of({}),
@@ -21,7 +22,7 @@ describe('SearchFormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [SearchFormComponent],
+      declarations: [SearchFormComponent, AppAriaDisabled],
       imports: [HttpClientTestingModule, FundamentalNgxModule, FormsModule],
       providers: [
         SearchService,
