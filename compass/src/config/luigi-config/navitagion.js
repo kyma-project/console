751d1effa257a8a3fc2d0f9833be8a3fecca79ac
dariadomagala <daria.domagala@sap.com>
2019-07-04 15:21:02 +0200

    Draft

--- compass/src/config/luigi-config/navitagion.js
+++ compass/src/config/luigi-config/navitagion.js
@@ -1,3 +1,16 @@
+const getTenants = () => {
+  const tenants = [{
+    category: 'Tenants',
+    label: 'Tenant 1',
+    pathValue: 'tenant1'
+  }, {
+    category: 'Tenants',
+    label: 'Tenant 2',
+    pathValue: 'tenant2'
+  }];
+  return tenants
+}
+
 const navigation = {
   nodes: () => [
     {
@@ -6,27 +19,55 @@ const navigation = {
       label: 'Overview',
       viewUrl: 'http://localhost:3000/',
     },
-    {
-      hideSideNav: true,
-      pathSegment: 'runtimes',
-      label: 'Runtimes',
-      viewUrl: 'http://localhost:3000/runtimes',
-    },
 
     {
       hideSideNav: true,
       hideFromNav: true,
-      pathSegment: 'runtime',
+      pathSegment: 'tenant',
+      label: 'Tenants',
       children: [
         {
           hideSideNav: true,
-          hideFromNav: true,
-          pathSegment: ':runtimeId',
-          label: 'Runtimes',
-          viewUrl: 'http://localhost:3000/runtime/:runtimeId',
-        },
-      ],
-    },
+          pathSegment: ':tenantId',
+          label: 'Tenant',
+          navigationContext: 'tenant',
+          context: {
+            tenantId: ':tenantId'
+          },
+          children: [
+            {
+              hideSideNav: true,
+              pathSegment: 'runtimes',
+              label: 'Runtimes',
+              viewUrl: 'http://localhost:3000/runtimes',
+            },
+            
+            {
+              hideSideNav: true,
+              hideFromNav: true,
+              pathSegment: 'runtime',
+              children: [
+                {
+                  hideSideNav: true,
+                  hideFromNav: true,
+                  pathSegment: ':runtimeId',
+                  label: 'Runtimes',
+                  viewUrl: 'http://localhost:3000/runtime/:runtimeId',
+                },
+              ],
+            },
+          ]
+        }
+      ]
+    }
   ],
+
+  contextSwitcher: {
+    defaultLabel: 'Select Tenant...',
+    parentNodePath: '/tenant', 
+    lazyloadOptions: true, // load options on click instead on page load
+    options: getTenants,
+  }
 };
+
 export default navigation;
--- compass/src/index.js
+++ compass/src/index.js
@@ -6,20 +6,30 @@ import * as serviceWorker from './serviceWorker';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import { COMPASS_GRAPHQL_ENDPOINT } from './config/config';
-import LuigiClient from '@kyma-project/luigi-client'; // eslint-disable-line no-unused-vars
+import builder from './commons/builder';
+import { BrowserRouter } from 'react-router-dom';
 
-const gqlClient = new ApolloClient({
+console.log(builder.getTenant())
+const client = new ApolloClient({
   uri: COMPASS_GRAPHQL_ENDPOINT,
+  // headers: {
+  //   tenant: builder.getTenant()
+  // }
 });
 
-ReactDOM.render(
-  <ApolloProvider client={gqlClient}>
-    <App />
-  </ApolloProvider>,
-  document.getElementById('root'),
-);
+(async () => {
+  await builder.init();
+  ReactDOM.render(
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </BrowserRouter>,
+    document.getElementById('root'),
+  );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  // If you want your app to work offline and load faster, you can change
+  // unregister() to register() below. Note this comes with some pitfalls.
+  // Learn more about service workers: https://bit.ly/CRA-PWA
+  serviceWorker.unregister();
+})();
