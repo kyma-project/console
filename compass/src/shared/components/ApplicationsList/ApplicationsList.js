2a0998a1ca10fb337ce8426f919a6c5a3a4caef0
parostatkiem <jan.sudczak@sap.com>
2019-07-10 14:15:00 +0200

    Add comments to propTypes

--- compass/src/shared/components/ApplicationsList/ApplicationsList.js
+++ compass/src/shared/components/ApplicationsList/ApplicationsList.js
@@ -16,12 +16,12 @@ const ApplicationsList = ({ entries, headerRenderer, rowRenderer }) =>
       notFoundMessage={"There are no applications available"}
     />
   ) : (
-    <h1>No elements {entries}</h1>
+    <h1>No elements</h1>
   );
 ApplicationsList.propTypes = {
-  entries: PropTypes.arrayOf(PropTypes.object),
-  headerRenderer: PropTypes.func.isRequired,
-  rowRenderer: PropTypes.func.isRequired
+  entries: PropTypes.arrayOf(PropTypes.object), // an array of objects to display
+  headerRenderer: PropTypes.func.isRequired, // gets the whole data as a param. Must return an array of nodes
+  rowRenderer: PropTypes.func.isRequired // gets a single row as a param. Must return an array of nodes
 };
 
 export default ApplicationsList;
