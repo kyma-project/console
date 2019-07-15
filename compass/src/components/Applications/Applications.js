a795bd0787b9cfc512876f5ea1a3883d5a9a3a29
parostatkiem <jan.sudczak@sap.com>
2019-07-15 10:31:42 +0200

    Add Actions param to GenericList

--- compass/src/components/Applications/Applications.js
+++ compass/src/components/Applications/Applications.js
@@ -1,30 +1,55 @@
 import React from "react";
-
+import { Token } from "fundamental-react/lib/Token";
 import GenericList from "../../shared/components/GenericList/GenericList";
 import { Query } from "react-apollo";
 import { GET_APPLICATIONS } from "./gql";
+import LuigiClient from "@kyma-project/luigi-client";
 
 class Applications extends React.Component {
-
-  createLabels = (labels) => {
+  createLabels = labels => {
     const separatedLabels = [];
     for (const key in labels) {
       if (labels.hasOwnProperty(key) && labels[key].length > 0) {
         labels[key].forEach(lab => {
-        if (lab === 'undefined') {
-          separatedLabels.push(key);
-        } else {
-          separatedLabels.push(key + '=' + lab);
-        } 
-      });
+          if (lab === "undefined") {
+            separatedLabels.push(key);
+          } else {
+            separatedLabels.push(key + "=" + lab);
+          }
+        });
       }
     }
-    const labelsString = separatedLabels.join(', ')
-    return labelsString;
-  }
+    return separatedLabels.map((label, id) => (
+      <Token key={id} className="y-fd-token y-fd-token--no-button y-fd-token--gap">
+        {label}
+      </Token>
+    ));
+  };
 
   headerRenderer = application => ["Name", "Description", "Labels"];
-  rowRenderer = application => [<b>{application.name}</b>, application.description, application.labels ? this.createLabels(application.labels) : '-'];
+  rowRenderer = application => [
+    <b>{application.name}</b>,
+    application.description,
+    application.labels ? this.createLabels(application.labels) : "-"
+  ];
+  actions = [
+    {
+      name: "Delete",
+      handler: entry => {
+        LuigiClient.uxManager()
+          .showConfirmationModal({
+            header: "Remove application",
+            body: `Are you sure you want to delete ${entry.name}?`,
+            buttonConfirm: "No",
+            buttonDismiss: "Also no"
+          })
+          .catch(() => {})
+          .finally(() => {
+            console.warn("As you wish, nothing will be removed");
+          });
+      }
+    }
+  ];
 
   render() {
     return (
@@ -35,12 +60,16 @@ class Applications extends React.Component {
           const apps = data.applications.data;
 
           return (
-            <GenericList entries={apps} headerRenderer={this.headerRenderer} rowRenderer={this.rowRenderer} />
-          )
+            <GenericList
+              actions={this.actions}
+              entries={apps}
+              headerRenderer={this.headerRenderer}
+              rowRenderer={this.rowRenderer}
+            />
+          );
         }}
       </Query>
     );
   }
-
 }
 export default Applications;
--- compass/src/shared/components/GenericList/GenericList.js
+++ compass/src/shared/components/GenericList/GenericList.js
@@ -1,10 +1,13 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import { Panel } from "fundamental-react/lib/Panel";
+import { Popover } from "fundamental-react/lib/Popover";
+import { Menu } from "fundamental-react/lib/Menu";
+import { Button } from "fundamental-react/lib/Button";
 import { Search, Filter, TableWithActionsToolbar, TableWithActionsList } from "@kyma-project/react-components";
 
 class GenericList extends React.Component {
-
   constructor(props) {
     super(props);
 
@@ -14,47 +17,66 @@ class GenericList extends React.Component {
     };
   }
 
-createLabels = (labels) => {
-  const separatedLabels = [];
-  for (const key in labels) {
-    if (labels.hasOwnProperty(key) && labels[key].length > 0) {
-      labels[key].forEach(lab => {
-      if (lab === 'undefined') {
-        separatedLabels.push(key);
-      } else {
-        separatedLabels.push(key + '=' + lab);
-      } 
-    });
+  processActionElement = (actions, entry) => (
+    <Popover
+      body={
+        <Menu>
+          <Menu.List>
+            {actions.map((
+              action,
+              id // no unique key error appears here. 'key' is not passed further by Fd-react
+            ) => (
+              <Menu.Item onClick={() => action.handler(entry)} key={(entry.name || entry.id) + id}>
+                {action.name}
+              </Menu.Item>
+            ))}
+          </Menu.List>
+        </Menu>
+      }
+      control={<Button glyph="vertical-grip" option="light" />}
+      placement="bottom-end"
+    />
+  );
+
+  headerRenderer = entries => {
+    if (this.props.actions) {
+      return [...this.props.headerRenderer(entries), ""];
+    } else {
+      return this.props.headerRenderer(entries);
     }
-  }
-  const labelsString = separatedLabels.join(', ')
-  return labelsString;
-}
+  };
 
-headerRenderer = application => ["Name", "Description", "Label"];
-rowRenderer = application => [<b>{application.name}</b>, application.description, application.labels ? this.createLabels(application.labels) : '-'];
+  rowRenderer = entry => {
+    if (this.props.actions) {
+      return [...this.props.rowRenderer(entry), this.processActionElement(this.props.actions, entry)];
+    } else {
+      return this.props.rowRenderer(entry);
+    }
+  };
 
-render() {
-    const apps = this.props.entries;
-     
-    const actions = (apps) => (
+  render() {
+    const { entries: apps } = this.props;
+
+    const headerActions = apps => (
       <>
-        <Filter
-          data={apps}
-          allFilters={[]}
-          activeFilters={[]}
-          onChange={() => {}}
-          />
-          <Search/>
+        <Filter data={apps} allFilters={[]} activeFilters={[]} onChange={() => {}} />
+        <Search />
       </>
-    )
+    );
 
     return (
       <Panel>
-        <TableWithActionsToolbar title="Applications" description="Description" children={actions(apps)} />
-        <TableWithActionsList entries={apps} headerRenderer={this.props.headerRenderer} rowRenderer={this.props.rowRenderer } />
+        <TableWithActionsToolbar title="Applications" description="Description" children={headerActions(apps)} />
+        <TableWithActionsList entries={apps} headerRenderer={this.headerRenderer} rowRenderer={this.rowRenderer} />
       </Panel>
     );
   }
 }
 export default GenericList;
+
+GenericList.propTypes = {
+  entries: PropTypes.arrayOf(PropTypes.object),
+  headerRenderer: PropTypes.func.isRequired,
+  rowRenderer: PropTypes.func.isRequired,
+  actions: PropTypes.arrayOf(PropTypes.shape({ name: PropTypes.string, handler: PropTypes.func }))
+};
