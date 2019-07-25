43a9d4993d77edc52fb937525240791ca60cca65
Kwiatosz, Krzysztof <kwiatekus@users.noreply.github.com>
2019-07-25 16:07:55 +0200

    Apply lint fixes

--- compass/src/components/Scenarios/gql.js
+++ compass/src/components/Scenarios/gql.js
@@ -2,8 +2,8 @@ import gql from 'graphql-tag';
 
 export const GET_SCENARIOS_LABEL_SCHEMA = gql`
   query {
-    labelDefinition(key : "scenarios"){
-      schema 
+    labelDefinition(key: "scenarios") {
+      schema
     }
   }
 `;
