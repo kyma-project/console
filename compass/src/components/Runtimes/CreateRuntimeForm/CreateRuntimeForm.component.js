af92ea457d0f46ee4d2bf63c29a42c5821e6c991
parostatkiem <jan.sudczak@sap.com>
2019-07-26 14:18:03 +0200

    Optimize useEffect

--- compass/src/components/Runtimes/CreateRuntimeForm/CreateRuntimeForm.component.js
+++ compass/src/components/Runtimes/CreateRuntimeForm/CreateRuntimeForm.component.js
@@ -16,28 +16,26 @@ const CreateRuntimeForm = ({
     name: useRef(null),
     description: useRef(null),
   };
-
   const [runtimeNames, setRuntimeNames] = useState([]);
 
   useEffect(() => {
-    if (Array.isArray(runtimeNames) && !runtimeNames.length)
-      fetchRuntimeNames();
-  });
+    const fetchRuntimeNames = async () => {
+      const allRuntimes = await getRuntimeNames.refetch();
+      if (
+        !allRuntimes.errors &&
+        allRuntimes &&
+        allRuntimes.data &&
+        allRuntimes.data.runtimes &&
+        allRuntimes.data.runtimes.data
+      ) {
+        setRuntimeNames(allRuntimes.data.runtimes.data.map(r => r.name));
+      } else {
+        setRuntimeNames([]);
+      }
+    };
 
-  const fetchRuntimeNames = async () => {
-    const allRuntimes = await getRuntimeNames.refetch();
-    if (
-      !allRuntimes.errors &&
-      allRuntimes &&
-      allRuntimes.data &&
-      allRuntimes.data.runtimes &&
-      allRuntimes.data.runtimes.data
-    ) {
-      setRuntimeNames(allRuntimes.data.runtimes.data.map(r => r.name));
-    } else {
-      setRuntimeNames(null);
-    }
-  };
+    fetchRuntimeNames();
+  }, [getRuntimeNames]);
 
   const handleFormSubmit = async e => {
     e.preventDefault();
