51eecd22e1b2fe4eb2d6a40f57514b84bc289463
Mateusz Puczynski <mateusz.puczynski@sap.com>
2019-08-01 16:24:10 +0200

    Update styled-components

--- components/react/config/buildGlobalStyles.js
+++ components/react/config/buildGlobalStyles.js
@@ -1,10 +1,11 @@
+import { css } from 'styled-components';
 function buildGlobalStyles() {
-  return `
+  return css`
     @font-face {
       font-family: 'SAP-icons';
       src: local('SAP-icons'), url('fonts/SAP-icons.woff2') format('woff2'),
-      url('/fonts/SAP-icons.woff') format('woff'),
-      url('/fonts/SAP-icons.ttf') format('truetype');
+        url('/fonts/SAP-icons.woff') format('woff'),
+        url('/fonts/SAP-icons.ttf') format('truetype');
       font-weight: normal;
       font-style: normal;
     }
@@ -12,17 +13,15 @@ function buildGlobalStyles() {
       font-family: '72';
       font-style: normal;
       font-weight: 400;
-      src: local('72'),
-        url('fonts/72-Regular.woff2') format('woff2'),
+      src: local('72'), url('fonts/72-Regular.woff2') format('woff2'),
         url('fonts/72-Regular.woff') format('woff');
     }
-    
+
     @font-face {
       font-family: '72';
       font-style: normal;
       font-weight: 700;
-      src: local('72-Bold'),
-        url('fonts/72-Bold.woff2') format('woff2'),
+      src: local('72-Bold'), url('fonts/72-Bold.woff2') format('woff2'),
         url('fonts/72-Bold.woff') format('woff');
     }
   `;
--- components/react/src/components/ThemeWrapper/index.js
+++ components/react/src/components/ThemeWrapper/index.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import { ThemeProvider, injectGlobal } from 'styled-components';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
 
 import buildGlobalStyles from '../../../config/buildGlobalStyles';
 import configureFonts from '../../../config/configureFonts';
 import theme from '../../../config/theme';
 
-injectGlobal([buildGlobalStyles()]);
+const GlobalStyle = createGlobalStyle`${buildGlobalStyles()}`;
 configureFonts();
 
 export default class ThemeWrapper extends Component {
   render() {
     return (
-      <ThemeProvider theme={theme}>
-        <div>{this.props.children}</div>
-      </ThemeProvider>
+      <GlobalStyle>
+        <ThemeProvider theme={theme}>
+          <div>{this.props.children}</div>
+        </ThemeProvider>
+      </GlobalStyle>
     );
   }
 }
