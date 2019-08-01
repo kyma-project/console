1c2301b21d580fef1321abc600bd1fc46821b7b4
Kwiatosz, Krzysztof <kwiatekus@users.noreply.github.com>
2019-08-01 08:55:13 +0200

    handle default tenant

--- compass/src/config/config.js
+++ compass/src/config/config.js
@@ -2,4 +2,5 @@ window.clusterConfig = {
   microfrontendContentUrl: 'http://localhost:8888',
   graphqlApiUrl: 'https://compass-gateway.kyma.local/director/graphql',
   tenants: 'A B C',
+  defaultTenant: 'A',
 };
--- compass/src/config/luigi-config/helpers/navigation-helpers.js
+++ compass/src/config/luigi-config/helpers/navigation-helpers.js
@@ -1,9 +1,10 @@
 const getTenants = () => {
   const tenantsString = window.clusterConfig.tenants || '';
+  const defaultTenant = window.clusterConfig.defaultTenant || '';
   const tenantsUIDs = tenantsString.split(' ');
   const tenants = tenantsUIDs.map(tenantUId => {
     return {
-      label: tenantUId,
+      label: tenantUId === defaultTenant ? 'default' : tenantUId,
       pathValue: tenantUId,
     };
   });
