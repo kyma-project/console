4f6339688c814df6c4c0f21307dbfe7d45302826
Szymon Janota <szymon.janota@sap.com>
2019-08-05 11:05:32 +0200

    Extract api link to new component and use in missing places

--- core/src/app/shared/services/generic-helpers.service.ts
+++ core/src/app/shared/services/generic-helpers.service.ts
@@ -1,7 +1,14 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {AppConfig} from '../../app.config';
 
 @Injectable()
 export class GenericHelpersService {
-  public getHostnameURL = (hostname: string, domain = ''): string =>
-    `https://${hostname}${domain}`;
+  public getURL = ({schema = 'https', host, domain = AppConfig.domain, port = '', path = '/'}: { schema?: string, host: string, domain?: string, port?: string, path?: string }) => {
+    const hostname = this.addDomainIfMissing(host, domain);
+    const hostnameWithPort = port ? `${hostname}:${port}` : hostname;
+    return `${schema}://${hostnameWithPort}${path}`
+  };
+
+  private addDomainIfMissing = (hostname: string, domain: string): string =>
+    hostname.endsWith('.' + domain) ? hostname : `${hostname}.${domain}`;
 }
