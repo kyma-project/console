0a0eb970bc221b60647747eac83a2c5ee8dc6e56
y-kkamil <kamil.kupczynski@sap.com>
2019-08-06 17:13:14 +0200

    Revert "Run prettier"
    This reverts commit a926e6a9f1d53e03a14747175c335e27b9d79f05.

--- compass/src/App.component.js
+++ compass/src/App.component.js
@@ -64,11 +64,7 @@ class App extends React.Component {
             )}
           />
           <Route path="/scenarios" exact component={Scenarios} />
-          <Route
-            path="/metadatadefinitions"
-            exact
-            component={MetadataDefinitions}
-          />
+          <Route path="/metadatadefinitions" exact component={MetadataDefinitions} />
         </Router>
       </div>
     );
--- compass/src/components/MetadataDefinitions/gql.js
+++ compass/src/components/MetadataDefinitions/gql.js
@@ -1,10 +1,11 @@
 import gql from 'graphql-tag';
 
 export const GET_LABEL_DEFINITIONS = gql`
-  query {
-    labelDefinitions {
-      key
-      schema
-    }
+query {
+  labelDefinitions{
+    key
+    schema
   }
+}
 `;
+
