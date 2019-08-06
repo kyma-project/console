9d6d5be0381a44aabe089dcd93aa49316b62a748
Szymon Janota <szymon.janota@sap.com>
2019-08-06 14:50:55 +0200

    Add error handling

--- core/src/app/shared/directives/filter-all-on-selected/filter-all-on-selected.directive.ts
+++ core/src/app/shared/directives/filter-all-on-selected/filter-all-on-selected.directive.ts
@@ -1,21 +1,32 @@
-import {Directive, ViewContainerRef} from '@angular/core';
+import {Directive, ViewContainerRef, Inject} from '@angular/core';
+
+import {ComboboxComponent} from 'fundamental-ngx/lib/combobox/combobox.component';
 
 @Directive({
   selector: '[filterAllOnSelected]'
 })
 export class FilterAllOnSelectedDirective {
 
-  private _combobox = null;
+  private combobox: ComboboxComponent = null;
 
-  constructor(private _viewContainerRef: ViewContainerRef) {
-    // Because of fundamental-ngx structure we cannot import ComboboxComponent here, thus the magic
-    this._combobox = _viewContainerRef['_data'].componentView.component;
-    this._combobox.filterFn = this.filterFn;
+  constructor(@Inject(ComboboxComponent) private _viewContainerRef) {
+    if (
+      !_viewContainerRef['_data']
+      || !_viewContainerRef['_data'].componentView
+      || !_viewContainerRef['_data'].componentView.component
+      || !_viewContainerRef['_data'].componentView.component.filterFn
+    ) {
+      throw new Error("filterAllOnSelected can only be used wth fd-combobox")
+    }
+
+    // Because of webpack (https://github.com/angular/angular-cli/issues/13658) we cannot import ComboboxComponent here, thus the magic
+    this.combobox = _viewContainerRef['_data'].componentView.component;
+    this.combobox.filterFn = this.filterFn;
   }
 
   public filterFn = (content: string[], searchTerm: string): string[] => {
     const searchTermLower = searchTerm.toLocaleLowerCase();
-    return content.indexOf(this._combobox.inputTextValue) >= 0
+    return content.indexOf(this.combobox.inputTextValue) >= 0
       ? content
       : content.filter(term => term.toLocaleLowerCase().indexOf(searchTermLower) >= 0);
   };
