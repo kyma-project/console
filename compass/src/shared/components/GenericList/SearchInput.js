cf96eac993501da4f1161d35f6fcfca6a3a3ff07
Wawrzyn321 <Wawrzyn321@gmail.com>
2019-08-07 15:50:09 +0200

    Fix list error

---
+++ compass/src/shared/components/GenericList/SearchInput.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './style.scss';
+import { withDuplicatesRemoved } from './helpers';
+
+SearchInput.propTypes = {
+  searchTerm: PropTypes.string,
+  entries: PropTypes.arrayOf(PropTypes.object.isRequired),
+  handleQueryChange: PropTypes.func.isRequired,
+};
+
+export default function SearchInput({
+  searchTerm,
+  filteredEntries,
+  handleQueryChange,
+}) {
+  const [isSearchHidden, setSearchHidden] = React.useState(true);
+  const searchInputRef = React.useRef();
+
+  const renderSearchList = entries => {
+    const suggestions = getSearchSuggestions(entries);
+
+    if (!suggestions.length) {
+      return (
+        <li
+          key="no-entries"
+          className="fd-menu__item fd-menu__item--no-entries"
+        >
+          No entries found
+        </li>
+      );
+    }
+
+    return suggestions.map(suggestion => (
+      <li
+        onClick={() => handleQueryChange(suggestion)}
+        key={suggestion}
+        className="fd-menu__item"
+      >
+        {suggestion}
+      </li>
+    ));
+  };
+
+  const getSearchSuggestions = entries => {
+    const suggestions = entries
+      .flatMap(entry => {
+        const headers = ['name', 'description'];
+        return headers.map(header => {
+          const entryValue = entry[header];
+          if (entryValue && entryValue.includes(searchTerm)) {
+            return entryValue;
+          }
+        });
+      })
+      .filter(suggestion => suggestion);
+    return withDuplicatesRemoved(suggestions);
+  };
+
+  const searchInputChanged = e => {
+    const searchTerm = e.target.value;
+    handleQueryChange(searchTerm);
+  };
+
+  const openSearchList = () => {
+    const inputField = searchInputRef.current;
+    if (inputField !== document.activeElement) {
+      inputField.focus();
+    }
+  };
+
+  const setInputFocus = isFocused => () => {
+    setSearchHidden(!isFocused);
+  };
+
+  return (
+    <div className="fd-search-input generic-list-search">
+      <div className="fd-popover">
+        <div className="fd-popover__control">
+          <div className="fd-combobox-control">
+            <div className="fd-input-group fd-input-group--after">
+              <input
+                ref={searchInputRef}
+                type="text"
+                className="fd-input"
+                placeholder="Search..."
+                value={searchTerm}
+                onChange={searchInputChanged}
+                onFocus={setInputFocus(true)}
+                onBlur={setInputFocus(false)}
+              />
+              <span className="fd-input-group__addon fd-input-group__addon--after fd-input-group__addon--button">
+                <button
+                  className="fd-button--light sap-icon--search"
+                  onClick={openSearchList}
+                ></button>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div
+          className="fd-popover__body fd-popover__body--no-arrow"
+          aria-hidden={isSearchHidden}
+        >
+          <nav className="fd-menu">
+            <ul className="fd-menu__list">
+              {renderSearchList(filteredEntries)}
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
+}
--- compass/src/shared/components/GenericList/helpers.js
+++ compass/src/shared/components/GenericList/helpers.js
@@ -2,15 +2,25 @@ export const filterEntries = (entries, query) => {
   // TEMP
   if (
     entries[0] &&
-    entries[0].hasOwnProperty('name') &&
-    entries[0].hasOwnProperty('description')
+    (entries[0].hasOwnProperty('name') ||
+      entries[0].hasOwnProperty('description'))
   ) {
     return entries.filter(entry => {
       return (
-        entry.name.indexOf(query) > -1 || entry.description.indexOf(query) > -1
+        (entry.name && entry.name.indexOf(query) > -1) ||
+        (entry.description && entry.description.indexOf(query) > -1)
       );
     });
   } else {
     return entries;
   }
+};
+
+export const withDuplicatesRemoved = array => {
+  return array.reduce((list, current) => {
+    if (!list.some(entry => entry === current)) {
+      list.push(current);
+    }
+    return list;
+  }, []);
 };
