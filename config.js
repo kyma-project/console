53da13c9a10ff67a6ec0f2e3258510dfd12be068
parostatkiem <jan.sudczak@sap.com>
2019-09-11 16:09:29 +0200

    reset config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-var domain = 'arnold.cluster.stage.faros.kyma.cx';
-var localDomain = 'console-dev.arnold.cluster.stage.faros.kyma.cx';
+var domain = 'kyma.local';
+var localDomain = 'console-dev.kyma.local';
 
 window.clusterConfig = {
   graphqlApiUrl: 'https://console-backend.' + domain + '/graphql',
