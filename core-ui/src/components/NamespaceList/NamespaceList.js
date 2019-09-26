a1c95710339a9dbc7fe81dfa5c4251674efdf57c
Kwiatosz, Krzysztof <kwiatekus@users.noreply.github.com>
2019-09-26 15:04:12 +0200

    buggy subscriptions

--- core-ui/src/components/NamespaceList/NamespaceList.js
+++ core-ui/src/components/NamespaceList/NamespaceList.js
@@ -1,10 +1,79 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useQuery } from '@apollo/react-hooks';
-import { GET_NAMESPACES } from '../../queries/queries';
+import {
+  GET_NAMESPACES,
+  NAMESPACES_EVENT_SUBSCRIPTION,
+} from '../../queries/queries';
 import { Spinner } from '@kyma-project/react-components';
 
 export default function NamespaceList() {
-  const { data, error, loading } = useQuery(GET_NAMESPACES);
+  const showSystemNamespaces = Boolean(
+    localStorage.getItem('console.showSystemNamespaces'),
+  );
+  const { data, error, loading, subscribeToMore } = useQuery(GET_NAMESPACES, {
+    variables: {
+      showSystemNamespaces,
+    },
+  });
+
+  const handleNamespaceEvent = (obj = {}, event = {}) => {
+    const currentItems = obj.namespaces || [];
+    const targetItem = event.namespace;
+    if (!targetItem) {
+      return obj;
+    }
+
+    let result;
+    switch (event.type) {
+      case 'ADD':
+        result = [...currentItems, targetItem];
+        break;
+      case 'UPDATE':
+        const items = [...currentItems];
+        const idx = items.findIndex(i => i.name === targetItem.name);
+        if (idx === -1) {
+          // if the `ADD` event hasn't been received
+          result = [...currentItems, targetItem];
+          break;
+        }
+        items[idx] = targetItem;
+        result = items;
+        break;
+      case 'DELETE':
+        result = currentItems.filter(i => i.name !== targetItem.name);
+        break;
+      default:
+        result = items;
+        break;
+    }
+
+    if (result && result.length > 0) {
+      result.sort((a, b) => {
+        if (a.name < b.name) return -1;
+        if (a.name > b.name) return 1;
+        return 0;
+      });
+    }
+
+    return { ...obj, namespaces: result };
+  };
+  useEffect(() => {
+    subscribeToMore({
+      variables: {
+        showSystemNamespaces,
+      },
+      document: NAMESPACES_EVENT_SUBSCRIPTION,
+      updateQuery: (prev, { subscriptionData }) => {
+        console.log('updateQuery', prev);
+        console.log('updateQuery', subscriptionData);
+        if (!subscriptionData.data || !subscriptionData.data.namespaceEvent) {
+          return prev;
+        }
+
+        return handleNamespaceEvent(prev, subscriptionData.data.namespaceEvent);
+      },
+    });
+  }, [subscribeToMore]);
 
   if (error) {
     return <p>Nie pykło</p>;
--- core-ui/src/queries/queries.js
+++ core-ui/src/queries/queries.js
@@ -1,13 +1,30 @@
 import gql from 'graphql-tag';
 
 export const GET_NAMESPACES = gql`
-  query {
-    namespaces(withSystemNamespaces: true) {
+  query Namespaces($showSystemNamespaces: Boolean) {
+    namespaces(withSystemNamespaces: $showSystemNamespaces) {
       name
       pods {
         status
       }
       applications
+      isSystemNamespace
+    }
+  }
+`;
+
+export const NAMESPACES_EVENT_SUBSCRIPTION = gql`
+  subscription Namespaces($showSystemNamespaces: Boolean) {
+    namespaceEvent(withSystemNamespaces: $showSystemNamespaces) {
+      type
+      namespace {
+        name
+        pods {
+          status
+        }
+        applications
+        isSystemNamespace
+      }
     }
   }
 `;
