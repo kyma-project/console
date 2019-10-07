f23250d1accaa85f42de1a5438c685f8f99248e9
dariadomagala <daria.domagala@sap.com>
2019-10-07 10:38:44 +0200

    Create draft labels component pt 2

--- core-ui/src/components/Labels/Labels.js
+++ core-ui/src/components/Labels/Labels.js
@@ -1,68 +1,97 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FormInput } from 'fundamental-react';
+import {
+  FormItem,
+  FormLabel,
+  FormInput,
+  Token,
+  FormMessage,
+} from 'fundamental-react';
 
 LabelsInput.propTypes = {
   labels: PropTypes.object,
   updateLabels: PropTypes.func,
-  updateErrorMessage: PropTypes.func,
 };
 
-export default function LabelsInput({
-  labels,
-  updateLabels,
-  updateErrorMessage,
-}) {
+export default function LabelsInput({ labels, updateLabels }) {
+  const [errorMessage, setErrorMessage] = React.useState('');
+
   function validateLabel(event) {
     const newLabel = event.target.value.trim();
     if (newLabel === '') return;
-    const separator = '=';
-    if (!(newLabel.split(separator).length === 2)) {
-      updateErrorMessage(
+    if (!(newLabel.split('=').length === 2)) {
+      setErrorMessage(
         `Invalid label '${newLabel}'. A key and value should be separated by a '='`,
       );
       return;
     }
 
     const regex = /([A-Za-z0-9][-A-Za-z0-9_.]*)?[A-Za-z0-9]/;
-    const key = newLabel.split(separator)[0];
-    const value = newLabel.split(separator)[1];
+    const key = newLabel.split('=')[0];
+    const value = newLabel.split('=')[1];
     const foundKey = key.match(regex);
     const foundVal = value.match(regex);
     const isKeyValid = foundKey && foundKey[0] === key && key !== '';
     const isValueValid = (foundVal && foundVal[0] === value) || value === '';
     if (!isKeyValid || !isValueValid) {
-      updateErrorMessage(
-        `Invalid label '${key}${separator}${value}'. In a valid label, a key cannot be empty, a key/value consists of alphanumeric characters, '-', '_' or '.', and must start and end with an alphanumeric character.`,
+      setErrorMessage(
+        `Invalid label '${key}=${value}'. In a valid label, a key cannot be empty, a key/value consists of alphanumeric characters, '-', '_' or '.', and must start and end with an alphanumeric character.`,
       );
       return;
     }
     if (labels[key]) {
-      updateErrorMessage(`Label with key value '${key}' already exists.`);
+      setErrorMessage(`Label with key '${key}' already exists.`);
       return;
     }
 
     const newLabels = { ...labels };
     newLabels[key] = value;
     updateLabels(newLabels);
-    updateErrorMessage('');
+    setErrorMessage('');
     event.target.value = '';
   }
 
+  function createLabelsToDisplay() {
+    const labelsArray = [];
+    for (const key in labels) {
+      const value = labels[key];
+      const labelToDisplay = `${key}=${value}`;
+      labelsArray.push(labelToDisplay);
+    }
+    return labelsArray;
+  }
+
+  function deleteLabel(event) {
+    const labelToDisplay = event.target.innerHTML;
+    const key = labelToDisplay.split('=')[0];
+    const newLabels = { ...labels };
+    delete newLabels[key];
+    updateLabels(newLabels);
+  }
+
   return (
-    <FormInput
-      id="labels"
-      type="text"
-      placeholder="Add labels"
-      autoComplete="off"
-      onKeyDown={e => {
-        if (e.key === 'Enter' || e.key === ' ') {
+    <FormItem key="labels">
+      <FormLabel htmlFor="labels">Labels</FormLabel>
+
+      {createLabelsToDisplay().map(label => (
+        <Token onClick={e => deleteLabel(e)}>{label}</Token>
+      ))}
+
+      <FormInput
+        type="text"
+        placeholder="Add new label"
+        autoComplete="off"
+        onKeyDown={e => {
+          if (e.key === 'Enter' || e.key === ' ') {
+            validateLabel(e);
+          }
+        }}
+        onBlur={e => {
           validateLabel(e);
-        }
-      }}
-      onBlur={e => {
-        validateLabel(e);
-      }}
-    />
+        }}
+      />
+
+      {errorMessage && <FormMessage type="error">{errorMessage}</FormMessage>}
+    </FormItem>
   );
 }
--- core-ui/src/components/Lambdas/Lambdas.js
+++ core-ui/src/components/Lambdas/Lambdas.js
@@ -1,30 +1,16 @@
 import React from 'react';
-import { FormItem, FormLabel } from 'fundamental-react';
+import { FormItem, FormLabel, FormMessage } from 'fundamental-react';
 
 import LabelsInput from '../Labels/Labels';
 
 export default function LambdasComponent() {
   const [labels, setLabels] = React.useState({ test: 'sss' });
-  const [errorMessage, setErrorMessage] = React.useState('');
 
   function updateLabels(newLabels) {
     setLabels(newLabels);
   }
 
-  function updateErrorMessage(error) {
-    setErrorMessage(error);
-  }
-
   return (
-    <FormItem key="labels">
-      <FormLabel htmlFor="labels">Labels</FormLabel>
-      {JSON.stringify(labels)}
-      <LabelsInput
-        labels={labels}
-        updateLabels={updateLabels}
-        updateErrorMessage={updateErrorMessage}
-      ></LabelsInput>
-      {errorMessage}
-    </FormItem>
+    <LabelsInput labels={labels} updateLabels={updateLabels}></LabelsInput>
   );
 }
