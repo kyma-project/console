5e43d1c0d1b1496d7f5f727e4cdcfd75a0783936
Wawrzyn321 <Wawrzyn321@gmail.com>
2019-10-08 14:17:22 +0200

    Fix Panel ::after issue

--- core-ui/src/components/NamespaceList/NamespacesListHeader/NamespacesListHeader.js
+++ core-ui/src/components/NamespaceList/NamespacesListHeader/NamespacesListHeader.js
@@ -20,24 +20,28 @@ export default function NamespacesListHeader({
   setLabelFilters,
 }) {
   return (
-    <Panel className="namespace-list-header fd-has-padding-medium remove-after">
-      <span className="fd-has-type-4">Namespaces</span>
-      <div className="namespace-list-actions">
-        <Search
-          onChange={e => updateSearchPhrase(e.target.value)}
-          data-test-id="namespace-seach-input"
-        />
-        <NamespaceFilters
-          filters={labelFilters}
-          updateFilters={setLabelFilters}
-        />
-        <ModalWithForm
-          title="Add new namespace"
-          button={{ text: 'Add new namespace', glyph: 'add' }}
-          id="add-namespace-modal"
-          renderForm={props => <CreateNamespaceForm {...props} />}
-        />
-      </div>
-    </Panel>
+    <div>
+      {' '}
+      {/* wrap in div to remove .fd-panel::after */}
+      <Panel className="namespace-list-header fd-has-padding-medium">
+        <span className="fd-has-type-4">Namespaces</span>
+        <div className="namespace-list-actions">
+          <Search
+            onChange={e => updateSearchPhrase(e.target.value)}
+            data-test-id="namespace-seach-input"
+          />
+          <NamespaceFilters
+            filters={labelFilters}
+            updateFilters={setLabelFilters}
+          />
+          <ModalWithForm
+            title="Add new namespace"
+            button={{ text: 'Add new namespace', glyph: 'add' }}
+            id="add-namespace-modal"
+            renderForm={props => <CreateNamespaceForm {...props} />}
+          />
+        </div>
+      </Panel>
+    </div>
   );
 }
