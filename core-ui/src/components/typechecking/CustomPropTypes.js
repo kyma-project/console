dfee60ae9afb65433cf6f1d299d141ead3c97dd5
parostatkiem <jan.sudczak@sap.com>
2019-10-09 14:02:30 +0200

    Fix CustomPropTypes error

--- core-ui/src/components/typechecking/CustomPropTypes.js
+++ core-ui/src/components/typechecking/CustomPropTypes.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 
 //TODO: move this component to a shared "place"
-export const elementRef = PropTypes.shape({ current: PropTypes.element });
+export const elementRef = PropTypes.shape({ current: PropTypes.any });
 
 export default {
   elementRef,
