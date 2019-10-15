f9fc29df56935e02c229e93308facaf9cb2d3985
parostatkiem <jan.sudczak@sap.com>
2019-10-15 13:37:18 +0200

    Load clusterConfig to luigiConfig in Core

--- core/webpack-generateConfig.config.js
+++ core/webpack-generateConfig.config.js
@@ -1,22 +1,37 @@
 const path = require('path');
+const webpack = require('webpack');
+
+function processConfigEnvVariables(sourceObject, prefix) {
+  const result = {}
+  for (var prop in sourceObject) {
+    if (prop.startsWith(prefix)) {
+      result[prop.replace(prefix,'')] =  JSON.stringify(sourceObject[prop]);
+    }
+  }
+  return result;
+}
 
 module.exports = {
-  mode: 'production',
-  entry: {
-    luigiConfig: './src/luigi-config/luigi-config.js'
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'src/assets')
-  },
-  module: {
-    rules: [
-      {
-        loader: 'babel-loader',
-        options: {
-          rootMode: 'root'
+    mode: 'production',
+    entry: {
+      luigiConfig: './src/luigi-config/luigi-config.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'src/assets')
+    },
+    module: {
+      rules: [
+        {
+          loader: 'babel-loader',
+          options: {
+            rootMode: 'root'
+          }
         }
-      }
-    ]
-  }
-};
+      ]
+    },
+    plugins:[
+      new webpack.DefinePlugin({'window.clusterConfig': processConfigEnvVariables(process.env,'REACT_APP_')})
+    ],
+  };
+
