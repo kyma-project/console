d6ee530d5d5bcd3f6f1fc15f4309c4a6732acb73
parostatkiem <jan.sudczak@sap.com>
2019-10-16 13:52:18 +0200

    Introduce env variables to Lambdas

--- lambda/src/app/app.config.ts
+++ lambda/src/app/app.config.ts
@@ -1,12 +1,5 @@
 import { environment } from '../environments/environment';
 
-const defaultDomain = 'kyma.local';
-let domain = defaultDomain;
-const clusterConfig: object = window['clusterConfig'];
-if (clusterConfig && clusterConfig['domain']) {
-  domain = clusterConfig['domain'];
-}
-
 const defaultFunctionSizesConf = {
   hpaCpuTargetAverageUtilization: 40,
   functionResources: { requests: { cpu: '100m', memory: '100Mi' } },
@@ -54,59 +47,56 @@ const defaultFunctionSizesConf = {
   ],
 };
 
-let functionSizes = defaultFunctionSizesConf['functionSizes'];
-if (clusterConfig && clusterConfig['functionSizes']) {
-  functionSizes = clusterConfig['functionSizes'];
-}
-
-let functionResourceRequest = defaultFunctionSizesConf['functionResources'];
-if (clusterConfig && clusterConfig['functionResources']) {
-  functionResourceRequest = clusterConfig['functionResources'];
-}
-
-let targetAverageUtilization =
-  defaultFunctionSizesConf['hpaCpuTargetAverageUtilization'];
-if (clusterConfig && clusterConfig['hpaCpuTargetAverageUtilization']) {
-  targetAverageUtilization = clusterConfig['hpaCpuTargetAverageUtilization'];
-}
-
 const defaultSubscriptionConfSpec = {
   includeSubscriptionNameHeader: true,
   eventType: '',
   eventTypeVersion: '',
 };
 
-let subscriptionConfigSpec = defaultSubscriptionConfSpec;
-if (clusterConfig && clusterConfig['subscriptionConfig']) {
-  subscriptionConfigSpec = clusterConfig['subscriptionConfig'];
-}
 
+const clusterConfig = (window as any).clusterConfig;
+
+
+
+const domain = clusterConfig.domain;
+const gateway_kyma_project_io_version = clusterConfig.gateway_kyma_project_io_version;
 const k8sServerUrl = `https://apiserver.${domain}`;
 
 const config = {
   authIssuer: `https://dex.${domain}`,
-  k8sServerUrl,
   kubelessApiUrl: `${k8sServerUrl}/apis/kubeless.io/v1beta1`,
+  k8sServerUrl,
   k8sApiUrl: `${k8sServerUrl}/apis/apps/v1`,
   k8sApiServerUrl: `${k8sServerUrl}/api/v1`,
+  k8sApiServerUrl_apimanagement: `${k8sServerUrl}/apis/gateway.kyma-project.io/${gateway_kyma_project_io_version}/`,
+  k8sApiServerUrl_apps: `${k8sServerUrl}/apis/apps/v1/`,
+  k8sApiServerUrl_applications: `${k8sServerUrl}/apis/applicationconnector.kyma-project.io/v1alpha1/applications/`,
+  k8sApiServerUrl_servicecatalog: `${k8sServerUrl}/apis/servicecatalog.k8s.io/v1beta1/`,
+  k8sApiServerUrl_rbac: `${k8sServerUrl}/apis/rbac.authorization.k8s.io/v1/`,
+
+  metricsUrl: `https://grafana.${domain}/`,
   apisApiUrl: `${k8sServerUrl}/apis/gateway.kyma-project.io/v1alpha2`,
   serviceBindingUsageUrl: `${k8sServerUrl}/apis/servicecatalog.kyma-project.io/v1alpha1`,
   serviceCatalogApiUrl: `${k8sServerUrl}/apis/servicecatalog.k8s.io/v1beta1`,
-  subscriptionApiUrl: `${k8sServerUrl}/apis/eventing.kyma-project.io/v1alpha1`,
-  graphqlApiUrl: `https://console-backend.${domain}/graphql`,
   autoscalingUrl: `${k8sServerUrl}/apis/autoscaling/v1`,
-  domain,
-  functionSizes,
-  functionResourceRequest,
-  targetAverageUtilization,
-  subscriptionConfigSpec,
-  metricsUrl: `https://grafana.${domain}/`,
+
+  functionSizes: clusterConfig.functionSizes ||defaultFunctionSizesConf['functionSizes'] ,
+  functionResourceRequest:clusterConfig.functionResources || defaultFunctionSizesConf.functionResources,
+  targetAverageUtilization:clusterConfig.hpaCpuTargetAverageUtilization||defaultFunctionSizesConf.hpaCpuTargetAverageUtilization,
+  subscriptionConfigSpec:clusterConfig.subscriptionConfig ||defaultSubscriptionConfSpec,
+
+  headerTitle: '',
+  headerLogoUrl: '',
+  faviconUrl: 'favicon.ico',
+  kubeconfigGeneratorUrl: `https://configurations-generator.${domain}/kube-config`,
+  idpLogoutUrl: null,
+  dexFQDNUri: 'http://dex-service.kyma-system.svc.cluster.local:5556/keys',
+
+  ...clusterConfig,
+  graphqlApiUrl: environment.localApi ? clusterConfig.graphqlApiUrlLocal:clusterConfig.graphqlApiUrl,
+    subscriptionApiUrl: `${k8sServerUrl}/apis/eventing.kyma-project.io/v1alpha1`,
 };
 
-if (clusterConfig) {
-  config.graphqlApiUrl = environment.localApi
-    ? clusterConfig['graphqlApiUrlLocal']
-    : clusterConfig['graphqlApiUrl'];
-}
 
-export const AppConfig = { ...config };
+export const AppConfig = { ...config } as any;
+
