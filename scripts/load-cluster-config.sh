121bbcfe4f4db9e05d802db0853586fdfa44aee0
parostatkiem <jan.sudczak@sap.com>
2019-10-18 11:03:21 +0200

    Update load-cluster-config.sh file

--- scripts/load-cluster-config.sh
+++ scripts/load-cluster-config.sh
@@ -1,3 +1,13 @@
+CLUSTER_CONFIG_ORIGINAL="${BASH_SOURCE%/*}/../.clusterConfig.default"
+CLUSTER_CONFIG_GEN="${BASH_SOURCE%/*}/../.clusterConfig.gen"
+
+
+if [ -r $CLUSTER_CONFIG_GEN ]; then
+    CURRENT_CONFIG_FILE=$CLUSTER_CONFIG_GEN
+else
+    CURRENT_CONFIG_FILE=$CLUSTER_CONFIG_ORIGINAL
+fi
+
 set -o allexport
-source "${BASH_SOURCE%/*}/../.clusterConfig.default"
+source $CURRENT_CONFIG_FILE
 set +o allexport
