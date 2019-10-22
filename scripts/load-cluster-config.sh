a94ddd365dc7d511f69e70d177853ccf23f4f3c0
parostatkiem <jan.sudczak@sap.com>
2019-10-22 10:17:40 +0200

    sfsa

--- scripts/load-cluster-config.sh
+++ scripts/load-cluster-config.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-if [ -z "${BASH_SOURCE}" ]; then SCRIPTPATH=$0 else SCRIPTPATH=${BASH_SOURCE[0]} fi
+if [ -z "${BASH_SOURCE}" ]; then
+    SCRIPTPATH=$0
+else
+    SCRIPTPATH=${BASH_SOURCE[0]}
+fi
 
 SCRIPT_DIR="$( cd "$( dirname "${SCRIPTPATH}" )" >/dev/null 2>&1 && pwd )"
 CLUSTER_CONFIG_ORIGINAL="$SCRIPT_DIR/../.clusterConfig.default"
