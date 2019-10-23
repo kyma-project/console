19f76aef5f508577a5c6d74d58d704d68a1be673
parostatkiem <jan.sudczak@sap.com>
2019-10-23 10:25:38 +0200

    sadfghasdg

--- core/src/app/app.config.ts
+++ core/src/app/app.config.ts
@@ -5,8 +5,8 @@ interface StringMap {
 
 declare const INJECTED_CLUSTER_CONFIG: StringMap; // injected by webpack
 
-const clusterConfig = (window as any).clusterConfig;
-const configToRead: StringMap = clusterConfig !== {} && clusterConfig || typeof INJECTED_CLUSTER_CONFIG !== 'undefined' ? INJECTED_CLUSTER_CONFIG : { domain: 'kyma.local' }; // fallback for tests
+const clusterConfig = (window as any).clusterConfig !== {} && (window as any).clusterConfig || undefined;
+const configToRead: StringMap = clusterConfig || typeof INJECTED_CLUSTER_CONFIG !== 'undefined' && INJECTED_CLUSTER_CONFIG || { domain: 'kyma.local' }; // fallback for tests
 
 const domain = configToRead.domain;
 const gateway_kyma_project_io_version = configToRead.gateway_kyma_project_io_version;
