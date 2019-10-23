7b8416713e4c301b866f8842ce517770fd940450
parostatkiem <jan.sudczak@sap.com>
2019-10-23 14:02:39 +0200

    asdgasdg

--- scripts/load-cluster-config.sh
+++ scripts/load-cluster-config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ -z "${BASH_SOURCE}" ]; then
-    SCRIPTPATH=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)
+    SCRIPTPATH=$(dirname -- "$(rreadlink "$0")")
     echo "bash source is not defined. SCRIPTPATH=$SCRIPTPATH"
 else
     SCRIPTPATH=${BASH_SOURCE[0]}
@@ -8,6 +8,7 @@ else
 fi
 
 SCRIPT_DIR="$( cd "$( dirname "${SCRIPTPATH}" )" >/dev/null 2>&1 && pwd )"
+echo "SCRIPT_DIR=$SCRIPT_DIR"
 CLUSTER_CONFIG_ORIGINAL="$SCRIPT_DIR/../.clusterConfig.default"
 CLUSTER_CONFIG_GEN="$SCRIPT_DIR/../.clusterConfig.gen"
 
