fef51d6847e4594f534371f758bc8f750186a14d
parostatkiem <jan.sudczak@sap.com>
2019-10-25 14:20:56 +0200

    fhgfgdhfd

--- scripts/load-cluster-config.sh
+++ scripts/load-cluster-config.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-if [ false ]; then
-    SCRIPTPATH=$0
-else
-    SCRIPTPATH=${BASH_SOURCE[0]}
-fi
 
+SCRIPTPATH=${BASH_SOURCE[0]}
 SCRIPT_DIR="$( cd "$( dirname "${SCRIPTPATH}" )" >/dev/null 2>&1 && pwd )$1"
 CLUSTER_CONFIG_ORIGINAL="$SCRIPT_DIR/../.clusterConfig.default"
 CLUSTER_CONFIG_GEN="$SCRIPT_DIR/../.clusterConfig.gen"
@@ -16,5 +12,5 @@ else
 fi
 
 set -o allexport
-. $CURRENT_CONFIG_FILE
+source $CURRENT_CONFIG_FILE
 set +o allexport
