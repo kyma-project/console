c840ce3f77b089847006433cca95c7c460d89381
parostatkiem <jan.sudczak@sap.com>
2019-10-25 14:36:30 +0200

    Bring back SH fallback

--- scripts/load-cluster-config.sh
+++ scripts/load-cluster-config.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
+if [ false ]; then
+    SCRIPTPATH=$0
+else
+    SCRIPTPATH=${BASH_SOURCE[0]}
+fi
 SCRIPTPATH=${BASH_SOURCE[0]}
 SCRIPT_DIR="$( cd "$( dirname "${SCRIPTPATH}" )" >/dev/null 2>&1 && pwd )$1"
 CLUSTER_CONFIG_ORIGINAL="$SCRIPT_DIR/../.clusterConfig.default"
