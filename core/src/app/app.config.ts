e923cccee2eb152d65a53b3a7d4d6513e2266047
parostatkiem <jan.sudczak@sap.com>
2019-10-29 15:18:38 +0100

    Fix url injection in Core

--- core/src/app/app.config.ts
+++ core/src/app/app.config.ts
@@ -9,18 +9,19 @@ const clusterConfig = Object.keys(windowClusterConfig || {}).length ? windowClus
 const configToRead: StringMap = clusterConfig || (typeof INJECTED_CLUSTER_CONFIG !== 'undefined' ? INJECTED_CLUSTER_CONFIG : { domain: 'kyma.local' }); // fallback for tests
 
 const domain = configToRead.domain;
-const gateway_kyma_project_io_version = configToRead.gateway_kyma_project_io_version;
+const gateway_kyma_cx_api_version = configToRead.gateway_kyma_cx_api_version;
 const k8sServerUrl = `https://apiserver.${domain}`;
 
 const config = {
   authIssuer: `https://dex.${domain}`,
   k8sApiServerUrl: `${k8sServerUrl}/api/v1/`,
-  k8sApiServerUrl_apimanagement: `${k8sServerUrl}/apis/gateway.kyma-project.io/${gateway_kyma_project_io_version}/`,
+  k8sApiServerUrl_apimanagement: `${k8sServerUrl}/apis/gateway.kyma-project.io/${gateway_kyma_cx_api_version}/`,
   k8sApiServerUrl_apps: `${k8sServerUrl}/apis/apps/v1/`,
   k8sApiServerUrl_applications: `${k8sServerUrl}/apis/applicationconnector.kyma-project.io/v1alpha1/applications/`,
   k8sApiServerUrl_servicecatalog: `${k8sServerUrl}/apis/servicecatalog.k8s.io/v1beta1/`,
   k8sApiServerUrl_rbac: `${k8sServerUrl}/apis/rbac.authorization.k8s.io/v1/`,
   subscriptionsApiUrl: `wss://console-backend.${domain}/graphql`,
+  gateway_kyma_project_io_version: gateway_kyma_cx_api_version,
 
   headerTitle: '',
   headerLogoUrl: '',
