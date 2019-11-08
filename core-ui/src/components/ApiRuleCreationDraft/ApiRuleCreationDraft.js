548385c6b8544970014a91ca4aa45c6ac18af5b6
parostatkiem <jan.sudczak@sap.com>
2019-11-08 15:16:52 +0100

    add access strategy section

--- core-ui/src/components/ApiRuleCreationDraft/ApiRuleCreationDraft.js
+++ core-ui/src/components/ApiRuleCreationDraft/ApiRuleCreationDraft.js
@@ -8,9 +8,17 @@ import {
   FormItem,
   FormLabel,
   Panel,
+  Checkbox,
+  FormFieldset,
+  FormLegend,
+  Badge,
+  FormSelect,
+  FormSet,
+  FormRadioGroup,
 } from 'fundamental-react';
 import PanelEntry from '../../shared/components/PanelEntry/PanelEntry.component';
 import { HostWithPortInput } from 'react-shared';
+import './ApiRuleCreationDraft.scss';
 
 const ApiRuleCreationDraft = () => {
   return (
@@ -40,24 +48,90 @@ const ApiRuleCreationDraft = () => {
       </header>
 
       <section className="fd-section">
-        <Panel>
-          <Panel.Header>
-            <Panel.Head title="Service" />
-          </Panel.Header>
-          <Panel.Body>
-            <FormGroup>
-              <FormItem>
-                <FormLabel htmlFor="input-1">Name</FormLabel>
-                <FormInput placeholder="Field placeholder text" type="text" />
-              </FormItem>
-              <HostWithPortInput
-                hostInfo={{ placeholder: 'Host' }}
-                portInfo={{ placeholder: 'Port' }}
-                label="URL"
+        <LayoutGrid cols={1}>
+          <Panel>
+            <Panel.Header>
+              <Panel.Head title="Service" />
+            </Panel.Header>
+            <Panel.Body>
+              <FormGroup>
+                <LayoutGrid cols={2}>
+                  <FormItem>
+                    <FormLabel htmlFor="input-1">Name</FormLabel>
+                    <FormInput
+                      placeholder="Field placeholder text"
+                      type="text"
+                      value="hasselhoff-beach-rescue-service"
+                    />
+                  </FormItem>
+                  <HostWithPortInput
+                    hostInfo={{
+                      placeholder: 'Host',
+                      value: 'hasselhoff.beach-rescue.california.gov',
+                    }}
+                    portInfo={{ placeholder: 'Port', value: 6969 }}
+                    label="URL"
+                  />
+                </LayoutGrid>
+              </FormGroup>
+            </Panel.Body>
+          </Panel>
+
+          <Panel className="access-strategy">
+            <Panel.Header>
+              <Panel.Head title="Access strategy for path " />
+              &nbsp;
+              <FormInput
+                placeholder="Field placeholder text"
+                type="text"
+                value="/.*"
               />
-            </FormGroup>
-          </Panel.Body>
-        </Panel>
+              <Panel.Actions></Panel.Actions>
+            </Panel.Header>
+
+            <Panel.Body>
+              <FormGroup>
+                <LayoutGrid cols={2}>
+                  <FormItem>
+                    <FormLabel htmlFor="select-1">Type</FormLabel>
+                    <FormSelect id="select-1">
+                      <option value="1">Strategy one</option>
+                      <option value="2">Strategy two</option>
+                      <option value="3">Ask Pamela</option>
+                    </FormSelect>
+                  </FormItem>
+                  <FormFieldset>
+                    <FormLegend>Method:</FormLegend>
+                    <FormRadioGroup inline className="inline-radio-group">
+                      <Checkbox
+                        id="checkbox-4"
+                        name="checkbox-name-4"
+                        value="GET"
+                        checked
+                      />
+                      <Checkbox
+                        id="checkbox-5"
+                        name="checkbox-name-5"
+                        value="POST"
+                        checked
+                      />
+                      <Checkbox
+                        id="checkbox-6"
+                        name="checkbox-name-6"
+                        value="PUT"
+                      />
+                      <Checkbox
+                        id="checkbox-6"
+                        name="checkbox-name-6"
+                        value="DELETE"
+                      />
+                    </FormRadioGroup>
+                  </FormFieldset>
+                </LayoutGrid>
+              </FormGroup>
+            </Panel.Body>
+          </Panel>
+        </LayoutGrid>
       </section>
     </>
   );
