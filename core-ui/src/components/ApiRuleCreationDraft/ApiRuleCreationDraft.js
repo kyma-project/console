49bd5ef5f2012350018cfad315852effb5666950
parostatkiem <jan.sudczak@sap.com>
2019-11-18 11:12:44 +0100

    Move general settings to a separate panel

--- core-ui/src/components/ApiRuleCreationDraft/ApiRuleCreationDraft.js
+++ core-ui/src/components/ApiRuleCreationDraft/ApiRuleCreationDraft.js
@@ -29,7 +29,7 @@ const ApiRuleCreationDraft = () => {
       <header className="fd-has-background-color-background-2">
         <section className="fd-has-padding-regular fd-has-padding-bottom-none action-bar-wrapper">
           <section>
-            <ActionBar.Header title="KKtest" />
+            <ActionBar.Header title="multiple-rules" />
           </section>
           <ActionBar.Actions>
             <FloatingControls>
@@ -41,56 +41,36 @@ const ApiRuleCreationDraft = () => {
             </FloatingControls>
           </ActionBar.Actions>
         </section>
-        <LayoutGrid nogap cols={4} className="header-properties">
-          <PanelEntry
-            title="Name"
-            content={
-              <input
-                placeholder="Enter the name"
-                value="multiple-rules"
-                type="text"
-              />
-            }
-          />
-          <PanelEntry
-            title="Gateway"
-            content={
-              <input
-                placeholder="Enter the gateway"
-                type="text"
-                value="kyma-gateway.kyma-system.svc.cluster.local"
-              />
-            }
-          />
-
-          <PanelEntry
-            title="Service name"
-            content={
-              <input
-                placeholder="Enter the service name"
-                type="text"
-                value="foo-service"
-              />
-            }
-          />
-
-          <PanelEntry
-            title="Service URL"
-            content={
-              <HostWithPortInput
-                hostInfo={{
-                  placeholder: 'Host',
-                  value: 'foo4.kyma.local',
-                }}
-                portInfo={{ placeholder: 'Port', value: '8080' }}
-              />
-            }
-          />
-        </LayoutGrid>
       </header>
 
       <section className="fd-section api-rule-container">
         <LayoutGrid cols={1}>
+          <Panel>
+            <Panel.Header>
+              <Panel.Head title="General settings" />
+            </Panel.Header>
+            <Panel.Body>
+              <FormGroup>
+                <LayoutGrid cols="2">
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <input
+                      placeholder="Enter the name"
+                      value="multiple-rules"
+                      type="text"
+                    />
+                  </FormItem>
+                  <FormItem>
+                    <FormLabel>Service</FormLabel>
+                    <FormSelect value="1" id="select-1">
+                      <option value="1">foo-service</option>
+                      <option value="2">bar-service</option>
+                    </FormSelect>
+                  </FormItem>
+                </LayoutGrid>
+              </FormGroup>
+            </Panel.Body>
+          </Panel>
           <AccessStrategy selectedType={1} path="/favicon" />
           <AccessStrategy selectedType={2} path="/img" />
           <AccessStrategy selectedType={3} path="/headers" />
