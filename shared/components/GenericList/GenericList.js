4e2895b0fbd912a250afceb8054c13f31f3bd58c
parostatkiem <jan.sudczak@sap.com>
2019-12-03 09:28:04 +0100

    make GenericList functional component part 1

--- shared/components/GenericList/GenericList.js
+++ shared/components/GenericList/GenericList.js
@@ -1,120 +1,110 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
 import SearchInput from './SearchInput';
-import './style.scss';
 
 import { Panel } from 'fundamental-react/Panel';
 
-import { TableWithActionsList } from '@kyma-project/react-components';
+import {
+  TableWithActionsToolbar,
+  TableWithActionsList,
+  TabsHeader,
+} from '@kyma-project/react-components';
 
 import { filterEntries } from './helpers';
 import { renderActionElement } from './internalRenderers';
 
-export class GenericList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      entries: props.entries,
-      filteredEntries: props.entries,
-      searchQuery: '',
-    };
-  }
+export const GenericList = ({
+  entries,
+  actions,
+  title,
+  description,
+  headerRenderer,
+  rowRenderer,
+  notFoundMessage,
+  extraHeaderContent,
+  showSearchField,
+}) => {
+  const [filteredEntries, setFilteredEntries] = useState(entries);
+  const [searchQuery, setSearchQuery] = useState('');
 
-  headerRenderer = entries => {
-    if (this.props.actions) {
-      return [...this.props.headerRenderer(entries), ''];
+  const HeaderRenderer = entries => {
+    if (actions) {
+      return [...headerRenderer(entries), ''];
     } else {
-      return this.props.headerRenderer(entries);
+      return headerRenderer(entries);
     }
   };
 
-  rowRenderer = entry => {
-    const actions = this.props.actions
-      ? this.props.actions.filter(action =>
-          action.skipAction ? !action.skipAction(entry) : true,
-        )
-      : [];
-    if (actions.length > 0) {
+  const RowRenderer = entry => {
+    // const actions = actions
+    //   ? actions.filter(action =>
+    //       action.skipAction ? !action.skipAction(entry) : true,
+    //     )
+    //   : [];
+    if (actions) {
       return [
-        ...this.props.rowRenderer(entry),
-        renderActionElement(actions, entry),
+        ...rowRenderer(entry),
+        // renderActionElement(actions, entry),
       ];
     } else {
-      return this.props.rowRenderer(entry);
+      return rowRenderer(entry);
     }
   };
 
-  handleQueryChange = searchQuery => {
-    this.setState(prevState => ({
-      filteredEntries: filterEntries(
-        prevState.entries,
-        searchQuery,
-        this.props.textSearchProperties,
-      ),
-      searchQuery,
-    }));
-  };
+  useEffect(() => {
+    setFilteredEntries(filterEntries, searchQuery);
+  }, [searchQuery, setFilteredEntries]);
 
-  static getDerivedStateFromProps(nextProps, prevState) {
-    if (!_.isEqual(nextProps.entries, prevState.entries)) {
-      return {
-        filteredEntries: filterEntries(
-          nextProps.entries,
-          prevState.searchQuery,
-          nextProps.textSearchProperties,
-        ),
-        entries: nextProps.entries,
-      };
-    }
-    return null;
-  }
+  // static getDerivedStateFromProps(nextProps, prevState) {
+  //   if (!_.isEqual(nextProps.entries, prevState.entries)) {
+  //     return {
+  //       filteredEntries: filterEntries(
+  //         nextProps.entries,
+  //         prevState.searchQuery,
+  //       ),
+  //       entries: nextProps.entries,
+  //     };
+  //   }
+  //   return null;
+  // }
 
-  render() {
-    const { filteredEntries, searchQuery } = this.state;
-    const {
-      extraHeaderContent,
-      notFoundMessage,
-      showSearchField,
-      textSearchProperties,
-    } = this.props;
+  const headerActions = (
+    <>
+      {extraHeaderContent}
+      {showSearchField && (
+        <SearchInput
+          searchQuery={searchQuery}
+          filteredEntries={filteredEntries}
+          handleQueryChange={setSearchQuery}
+        />
+      )}
+    </>
+  );
 
-    const headerActions = (
-      <section>
-        {showSearchField && (
-          <SearchInput
-            searchQuery={searchQuery}
-            filteredEntries={filteredEntries}
-            handleQueryChange={this.handleQueryChange}
-            suggestionProperties={textSearchProperties}
-          />
-        )}
-        {extraHeaderContent}
-      </section>
-    );
+  return (
+    <Panel className="fd-panel--no-background">
+      <TableWithActionsToolbar
+        title={title}
+        description={description}
+        children={headerActions}
+      />
 
-    return (
-      <Panel className="fd-has-margin-m generic-list">
-        <Panel.Header className="fd-has-padding-xs">
-          <Panel.Head title={this.props.title} />
-          {headerActions}
-        </Panel.Header>
-
-        <Panel.Body>
-          <TableWithActionsList
-            notFoundMessage={notFoundMessage || 'There are no items to show'}
-            entries={filteredEntries}
-            headerRenderer={this.headerRenderer}
-            rowRenderer={this.rowRenderer}
-          />
-        </Panel.Body>
-      </Panel>
-    );
-  }
-}
+      <Panel.Body>
+        <TableWithActionsList
+          notFoundMessage={notFoundMessage || 'There are no items to show'}
+          entries={filteredEntries}
+          headerRenderer={HeaderRenderer}
+          rowRenderer={RowRenderer}
+        />
+      </Panel.Body>
+    </Panel>
+  );
+};
 
 GenericList.propTypes = {
-  title: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
   entries: PropTypes.arrayOf(PropTypes.object),
   headerRenderer: PropTypes.func.isRequired,
   rowRenderer: PropTypes.func.isRequired,
@@ -123,10 +113,9 @@ GenericList.propTypes = {
   ),
   extraHeaderContent: PropTypes.node,
   showSearchField: PropTypes.bool,
-  textSearchProperties: PropTypes.arrayOf(PropTypes.string.isRequired),
+  notFoundMessage: PropTypes.string,
 };
 
 GenericList.defaultProps = {
   showSearchField: true,
-  textSearchProperties: ['name', 'description'],
 };
---
+++ shared/components/GenericList/ListActions/ListActions.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ListActions = () => {
+  return 'aaa';
+};
+
+export default ListActions;
