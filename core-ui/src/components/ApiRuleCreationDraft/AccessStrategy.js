1fa705b23a066f68bcdbb028d0189b5d08b5445b
parostatkiem <jan.sudczak@sap.com>
2019-12-03 09:24:01 +0100

    Force https in access strategy input

--- core-ui/src/components/ApiRuleCreationDraft/AccessStrategy.js
+++ core-ui/src/components/ApiRuleCreationDraft/AccessStrategy.js
@@ -17,7 +17,7 @@ import {
 import { StringInput } from 'react-shared';
 
 const URLregexp = new RegExp(
-  '(https?://(?:www.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^s]{2,}|www.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^s]{2,}|https?://(?:www.|(?!www))[a-zA-Z0-9]+.[^s]{2,}|www.[a-zA-Z0-9]+.[^s]{2,})',
+  '(https://(?:www.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^s]{2,}|www.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^s]{2,}|https?://(?:www.|(?!www))[a-zA-Z0-9]+.[^s]{2,}|www.[a-zA-Z0-9]+.[^s]{2,})',
 );
 const StringListInput = ({
   typesFilter,
