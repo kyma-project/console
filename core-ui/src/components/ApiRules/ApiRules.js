4e88b79ba71f25261376885fe71699aa9c02fd04
parostatkiem <jan.sudczak@sap.com>
2019-12-12 16:04:38 +0100

    Add actions and details link

--- core-ui/src/components/ApiRules/ApiRules.js
+++ core-ui/src/components/ApiRules/ApiRules.js
@@ -3,15 +3,38 @@ import { PageHeader, GenericList, Spinner } from 'react-shared';
 import { GET_API_RULES } from 'gql/queries';
 import { useQuery } from '@apollo/react-hooks';
 import LuigiClient from '@kyma-project/luigi-client';
+import { Button } from 'fundamental-react';
 
 const ApiRules = () => {
   const headerRenderer = () => ['Name'];
 
-  const rowRenderer = rule => [rule.name];
+  const rowRenderer = rule => [
+    <span
+      className="link"
+      onClick={() => LuigiClient.linkManager().navigate(`details/${rule.name}`)}
+    >
+      {rule.name}
+    </span>,
+  ];
 
   const { loading, error, data } = useQuery(GET_API_RULES, {
     variables: { namespace: LuigiClient.getContext().namespaceId },
   });
+
+  const actions = [
+    {
+      name: 'Edit',
+      handler: entry => {
+        console.log('edit', entry);
+      },
+    },
+    {
+      name: 'Delete',
+      handler: entry => {
+        console.log('delete', entry);
+      },
+    },
+  ];
 
   if (error) {
     return `Error! ${error.message}`;
@@ -25,11 +48,19 @@ const ApiRules = () => {
     <>
       <PageHeader title="API rules" />
       <GenericList
-        //actions={actions}
+        actions={actions}
         entries={data.APIRules}
         headerRenderer={headerRenderer}
         rowRenderer={rowRenderer}
-        // extraHeaderContent={<CreateLambdaModal />}
+        extraHeaderContent={
+          <Button
+            glyph="add"
+            option="light"
+            onClick={() => LuigiClient.linkManager().navigate(`create`)}
+          >
+            Add new API rule
+          </Button>
+        }
       />
     </>
   );
