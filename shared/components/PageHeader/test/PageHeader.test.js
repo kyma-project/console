a1949f7878b84bc1feb498f9df0490a1a4b41004
Szymon Janota <szymon.janota@sap.com>
2019-12-27 06:21:33 +0100

    Remove commented import

--- shared/components/PageHeader/test/PageHeader.test.js
+++ shared/components/PageHeader/test/PageHeader.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-// import LuigiClient from '@kyma-project/luigi-client';
 
 import { PageHeader } from '../PageHeader';
 
