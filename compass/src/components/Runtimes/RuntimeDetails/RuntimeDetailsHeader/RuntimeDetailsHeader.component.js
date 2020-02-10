b7b59056b02a03b4f50e7a46c0c9318fbcf708e0
dariadomagala <daria.domagala@sap.com>
2020-02-10 09:26:19 +0100

    Small refactor to fix styles

--- compass/src/components/Runtimes/RuntimeDetails/RuntimeDetailsHeader/RuntimeDetailsHeader.component.js
+++ compass/src/components/Runtimes/RuntimeDetails/RuntimeDetailsHeader/RuntimeDetailsHeader.component.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Panel, PanelGrid } from '@kyma-project/react-components';
 import LuigiClient from '@kyma-project/luigi-client';
 
 import { PageHeader, StatusBadge, EMPTY_TEXT_PLACEHOLDER } from 'react-shared';
@@ -20,39 +19,22 @@ class RuntimeDetailsHeader extends React.Component {
 
   render = () => {
     const { name, description, id, status } = this.props.runtime;
-
-    const PanelEntry = props => {
-      return (
-        <Panel>
-          <Panel.Body>
-            <p className="fd-has-color-text-4 fd-has-margin-bottom-none">
-              {props.title}
-            </p>
-            {props.content}
-          </Panel.Body>
-        </Panel>
-      );
-    };
-
     const breadcrumbItems = [{ name: 'Runtimes', path: '/' }, { name: '' }];
 
     return (
-      <>
-        <PageHeader breadcrumbItems={breadcrumbItems} title={name} />
-        <PanelGrid nogap cols={3}>
-          {status && (
-            <PanelEntry
-              title="Status"
-              content={<StatusBadge status={status.condition} />}
-            />
-          )}
-          <PanelEntry
-            title="Description"
-            content={description ? description : EMPTY_TEXT_PLACEHOLDER}
-          />
-          <PanelEntry title="ID" content={id} />
-        </PanelGrid>
-      </>
+      <PageHeader breadcrumbItems={breadcrumbItems} title={name}>
+        {status && (
+          <PageHeader.Column title="Status" columnSpan={null}>
+            {<StatusBadge status={status.condition} />}
+          </PageHeader.Column>
+        )}
+        <PageHeader.Column title="Description" columnSpan={null}>
+          {description ? description : EMPTY_TEXT_PLACEHOLDER}
+        </PageHeader.Column>
+        <PageHeader.Column title="ID" columnSpan={null}>
+          {id}
+        </PageHeader.Column>
+      </PageHeader>
     );
   };
 }
