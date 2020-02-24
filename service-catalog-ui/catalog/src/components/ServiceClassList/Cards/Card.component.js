cc24c99c45f47886f7ae78e16eeb9973f2b13016
parostatkiem <jan.sudczak@sap.com>
2020-02-24 13:10:08 +0100

    Display the icon only if appropriate label is set

--- service-catalog-ui/catalog/src/components/ServiceClassList/Cards/Card.component.js
+++ service-catalog-ui/catalog/src/components/ServiceClassList/Cards/Card.component.js
@@ -19,6 +19,7 @@ import {
 const DOCUMENTATION_PER_PLAN_DESCRIPTION = `
 This service has different documentation and APIs for each plan
 `; //TODO: change
+const DOCUMENTATION_PER_PLAN_LABEL = 'documentation-per-plan';
 
 const Card = ({
   title,
@@ -28,46 +29,42 @@ const Card = ({
   numberOfInstances = 0,
   labels,
   onClick,
-}) => {
-  return (
-    <CardWrapper data-e2e-id="card" className="card">
-      <CardContent onClick={onClick} data-e2e-id={`go-to-details`}>
-        <CardTop>
-          <CardHeader className="card__header">
-            <CardThumbnail>
-              {imageUrl ? (
-                <CardImage size="s" photo={imageUrl} />
-              ) : (
-                <Icon
-                  glyph="crm-service-manager"
-                  style={{ color: '#515559' }}
-                />
-              )}
-            </CardThumbnail>
+}) => (
+  <CardWrapper data-e2e-id="card" className="card">
+    <CardContent onClick={onClick} data-e2e-id={`go-to-details`}>
+      <CardTop>
+        <CardHeader className="card__header">
+          <CardThumbnail>
+            {imageUrl ? (
+              <CardImage size="s" photo={imageUrl} />
+            ) : (
+              <Icon glyph="crm-service-manager" style={{ color: '#515559' }} />
+            )}
+          </CardThumbnail>
 
-            <CardHeaderContent data-e2e-id="card-title" title={title}>
-              <span data-e2e-id="card-company">{company}</span>
-            </CardHeaderContent>
-
+          <CardHeaderContent data-e2e-id="card-title" title={title}>
+            <span data-e2e-id="card-company">{company}</span>
+          </CardHeaderContent>
+          {labels[DOCUMENTATION_PER_PLAN_LABEL] === 'true' && (
             <Tooltip title={DOCUMENTATION_PER_PLAN_DESCRIPTION}>
               <div className="icon">
                 <Icon glyph="sap-box" size="l" />
               </div>
             </Tooltip>
+          )}
 
-            <InstancesIndicator
-              numberOfInstances={numberOfInstances}
-              labels={labels}
-            />
-          </CardHeader>
-        </CardTop>
+          <InstancesIndicator
+            numberOfInstances={numberOfInstances}
+            labels={labels}
+          />
+        </CardHeader>
+      </CardTop>
 
-        <CardDescription>{description}</CardDescription>
-        <Labels labels={labels} />
-      </CardContent>
-    </CardWrapper>
-  );
-};
+      <CardDescription>{description}</CardDescription>
+      <Labels labels={labels} />
+    </CardContent>
+  </CardWrapper>
+);
 
 Card.propTypes = {
   title: PropTypes.string.isRequired,
