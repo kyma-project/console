7963116f6af0abf0542bf07e018383159013cb3b
dariadomagala <daria.domagala@sap.com>
2020-03-12 11:37:58 +0100

    Cleanup intex js

--- shared/index.js
+++ shared/index.js
@@ -1,11 +1,13 @@
-import { GenericList } from './components/GenericList/GenericList';
-import { K8sNameInput } from './components/K8sNameInput/K8sNameInput';
+export { GenericList } from './components/GenericList/GenericList';
+export { K8sNameInput } from './components/K8sNameInput/K8sNameInput';
 export { StringInput } from './components/StringInput/StringInput';
 export { InputWithSuffix } from './components/InputWithSuffix/InputWithSuffix';
 export { InputWithPrefix } from './components/InputWithPrefix/InputWithPrefix';
-import { CollapsiblePanel } from './components/CollapsiblePanel/CollapsiblePanel';
-import { Tooltip } from './components/Tooltip/Tooltip';
-import { PageHeader } from './components/PageHeader/PageHeader';
+export {
+  CollapsiblePanel,
+} from './components/CollapsiblePanel/CollapsiblePanel';
+export { Tooltip } from './components/Tooltip/Tooltip';
+export { PageHeader } from './components/PageHeader/PageHeader';
 export { Spinner } from './components/Spinner/Spinner';
 export { CopiableText } from './components/CopiableText/CopiableText';
 export { DetailsError } from './components/DetailsError/DetailsError';
@@ -18,7 +20,6 @@ export {
 } from './components/ResourceNotFound/ResourceNotFound';
 export { StatusBadge } from './components/StatusBadge/StatusBadge';
 export { TextFormItem } from './components/TextFormItem/TextFormItem';
-import CustomPropTypes from './typechecking/CustomPropTypes';
 
 export * from './forms';
 export * from './hooks';
@@ -29,11 +30,6 @@ export * from './components/GenericList/actionHandlers/simpleDelete';
 export * from './components/CredentialForms';
 export * from './constants/constants';
 
-export {
-  CustomPropTypes,
-  GenericList,
-  K8sNameInput,
-  CollapsiblePanel,
-  Tooltip,
-  PageHeader,
-};
+import CustomPropTypes from './typechecking/CustomPropTypes';
+
+export { CustomPropTypes };
