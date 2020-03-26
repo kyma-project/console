4f6babf9c94a4df6f44f575bec6ae6e03a4592e4
Wawrzyn321 <Wawrzyn321@gmail.com>
2020-03-26 10:36:18 +0100

    Adjust merged changes

--- compass/src/components/TenantSearch/TenantSearch.js
+++ compass/src/components/TenantSearch/TenantSearch.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import LuigiClient from '@kyma-project/luigi-client';
+import LuigiClient from '@luigi-project/client';
 
 import fetchTenants from './fetchTenants';
 import { ListGroup, Panel } from 'fundamental-react';
