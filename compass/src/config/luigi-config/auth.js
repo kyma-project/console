210ea4ca989339cfab7846bdaf136203ac4597db
Wawrzyn321 <Wawrzyn321@gmail.com>
2020-03-26 10:23:42 +0100

    Fix dex config

--- compass/src/config/luigi-config/auth.js
+++ compass/src/config/luigi-config/auth.js
@@ -7,33 +7,41 @@ const authClusterConfig = window.clusterConfig && window.clusterConfig.auth;
 const clientId = authClusterConfig
   ? authClusterConfig['client_id']
   : 'compass-ui';
-const auth = {
-  use: 'openIdConnect',
-  openIdConnect: {
-    idpProvider: OpenIdConnect,
-    authority: authClusterConfig
-      ? authClusterConfig['authority']
-      : `https://dex.${domain}`,
-    client_id: clientId,
-    scope: authClusterConfig
-      ? authClusterConfig['scope']
-      : 'audience:server:client_id:compass-ui openid profile email groups',
-    loadUserInfo: false,
-    logoutUrl: '/logout.html',
-    profileStorageInterceptorFn: () => {
-      try {
-        const oidcUserStoreKey = `oidc.user:https://dex.${domain}:${clientId}`;
-        const oidsUserStore = JSON.parse(
-          sessionStorage.getItem(oidcUserStoreKey),
-        );
-        oidsUserStore.profile = undefined;
-        sessionStorage.setItem(oidcUserStoreKey, JSON.stringify(oidsUserStore));
-      } catch (e) {
-        console.error('Error parsing oidc user data', e);
-      }
-    },
-  },
-  storage: 'sessionStorage',
-};
 
-export default auth;
+export default function createAuth(dexMetadata) {
+  return {
+    use: 'openIdConnect',
+    openIdConnect: {
+      metadata: {
+        ...dexMetadata,
+        end_session_endpoint: 'logout.html',
+      },
+      idpProvider: OpenIdConnect,
+      authority: authClusterConfig
+        ? authClusterConfig['authority']
+        : `https://dex.${domain}`,
+      client_id: clientId,
+      scope: authClusterConfig
+        ? authClusterConfig['scope']
+        : 'audience:server:client_id:compass-ui openid profile email groups',
+      loadUserInfo: false,
+      logoutUrl: '/logout.html',
+      profileStorageInterceptorFn: () => {
+        try {
+          const oidcUserStoreKey = `oidc.user:https://dex.${domain}:${clientId}`;
+          const oidsUserStore = JSON.parse(
+            sessionStorage.getItem(oidcUserStoreKey),
+          );
+          oidsUserStore.profile = undefined;
+          sessionStorage.setItem(
+            oidcUserStoreKey,
+            JSON.stringify(oidsUserStore),
+          );
+        } catch (e) {
+          console.error('Error parsing oidc user data', e);
+        }
+      },
+    },
+    storage: 'sessionStorage',
+  };
+}
--- compass/src/config/luigi-config/main.js
+++ compass/src/config/luigi-config/main.js
@@ -1,12 +1,31 @@
 import settings from './settings';
 import navigation from './navigation';
-import auth from './auth';
+import createAuth from './auth';
 
-Luigi.setConfig({
-  navigation,
-  auth,
-  routing: {
-    useHashRouting: false,
-  },
-  settings,
-});
+async function fetchDexMetadata() {
+  const domain =
+    (window.clusterConfig && window.clusterConfig['domain']) || 'pijany.hasselhoff.ga';
+
+  try {
+    const response = await fetch(`https://dex.${domain}/.well-known/openid-configuration`);
+    return await response.json();
+  }
+  catch (e) {
+    alert('Cannot fetch dex metadata');
+    console.error('cannot fetch dex metadata', e);
+  }
+}
+
+
+(async () => {
+  const dexMetadata = await fetchDexMetadata();
+  
+  Luigi.setConfig({
+    navigation,
+    auth: createAuth(dexMetadata),
+    routing: {
+      useHashRouting: false,
+    },
+    settings,
+  });
+})();
