f8e9a1fc704d9e5fdbe0d16660260d32af48b7a5
Wawrzyn321 <Wawrzyn321@gmail.com>
2020-04-20 11:27:27 +0200

    Add more tests

--- tests/tests/tests.js
+++ tests/tests/tests.js
@@ -58,6 +58,74 @@ testIf(
   },
 );
 
+test('Cluster Addons view is rendered', async t => {
+  //GIVEN
+  const addonsLink = leftNavLinkSelector('Cluster Addons');
+  await t
+    .useRole(adminUser)
+    //WHEN
+    .click(addonsLink);
+
+  //THEN
+  await retry(t, 3, findActiveFrame);
+  await retry(t, 3, t => {
+    return t
+      .expect(
+        Selector('.fd-action-bar__title').withText(/Cluster Addons/i).exists,
+      )
+      .ok();
+  });
+});
+
+test('Lambda view is rendered', async t => {
+  //GIVEN
+  const lambdaLink = leftNavLinkSelector('Lambdas');
+  await t.useRole(adminUser);
+
+  await retry(t, 3, findActiveFrame);
+  await retry(t, 3, t => {
+    return t
+      .click(Selector('.fd-panel__title').withText('default'))
+      .switchToMainWindow()
+      .click(lambdaLink);
+  });
+
+  //THEN
+  await retry(t, 3, findActiveFrame);
+  await retry(t, 3, t => {
+    return t
+      .expect(Selector('.fd-action-bar__title').withText(/Lambdas/i).exists)
+      .ok();
+  });
+});
+
+test('Logs view is rendered', async t => {
+  //GIVEN
+  const logsLink = leftNavLinkSelector('Logs');
+  await t
+    .useRole(adminUser)
+    //WHEN
+    .click(logsLink);
+
+  //THEN
+  await retry(t, 3, findActiveFrame);
+  // check title
+  await retry(t, 3, t => {
+    return t.expect(Selector('h1').withText(/Logs/i).exists).ok();
+  });
+
+  // check loading log sources
+  await retry(t, 3, async t => {
+    await t.click(
+      Selector('input').withAttribute('placeholder', /Select Label/i),
+    );
+    await t.click(Selector('.fd-mega-menu__link').withText(/namespace/i));
+    return t
+      .expect(Selector('.fd-mega-menu__sublink').withText('default').exists)
+      .ok();
+  });
+});
+
 testIf(
   toBoolean(config.serviceCatalogEnabled),
   'Catalog view is rendered',
@@ -81,6 +149,71 @@ testIf(
       return t
         .expect(
           Selector('.fd-action-bar__title').withText('Service Catalog').exists,
+        )
+        .ok();
+    });
+  },
+);
+
+testIf(
+  toBoolean(config.serviceCatalogEnabled),
+  'Service Brokers view is rendered',
+  async t => {
+    //GIVEN
+    await t.useRole(adminUser);
+    const brokersLink = leftNavLinkSelector('Brokers');
+
+    //WHEN
+    await retry(t, 3, findActiveFrame);
+    await retry(t, 3, t => {
+      return t
+        .click(Selector('.fd-panel__title').withText('default'))
+        .switchToMainWindow()
+        .click(brokersLink);
+    });
+
+    //THEN
+    await retry(t, 3, findActiveFrame);
+
+    // check title
+    await retry(t, 3, t => {
+      return t
+        .expect(
+          Selector('.fd-action-bar__title').withText('Service Brokers').exists,
+        )
+        .ok();
+    });
+
+    // check loading brokers - helm-broker should be always preset in table
+    await retry(t, 3, t => {
+      return t.expect(Selector('td').withText('helm-broker').exists).ok();
+    });
+  },
+);
+
+testIf(
+  toBoolean(config.serviceCatalogEnabled),
+  'Instances view is rendered',
+  async t => {
+    //GIVEN
+    await t.useRole(adminUser);
+    const instancesLink = leftNavLinkSelector('Instance');
+
+    //WHEN
+    await retry(t, 3, findActiveFrame);
+    await retry(t, 3, t => {
+      return t
+        .click(Selector('.fd-panel__title').withText('default'))
+        .switchToMainWindow()
+        .click(instancesLink);
+    });
+
+    //THEN
+    await retry(t, 3, findActiveFrame);
+    await retry(t, 3, t => {
+      return t
+        .expect(
+          Selector('.fd-action-bar__title').withText('Service Instance').exists,
         )
         .ok();
     });
