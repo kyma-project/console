9c643d5acab8ed7a6961d81da9c7bb5b1dbbdc34
Szymon Janota <szymon.janota@sap.com>
2020-04-20 12:05:39 +0200

    Extract global app logic to the top

---
+++ service-catalog-ui/catalog/src/components/App/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Modal } from '@kyma-project/react-components';
+
+import { NotificationProvider } from '../../../shared/contexts/NotificationContext';
+import CatalogApp from '../catalog/App/App';
+
+Modal.MODAL_APP_REF = '#root';
+
+export default function App() {
+  return (
+    <NotificationProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/catalog" component={CatalogApp} />
+        </Switch>
+      </BrowserRouter>
+    </NotificationProvider>
+  );
+}
--- service-catalog-ui/catalog/src/components/catalog/App/App.js
+++ service-catalog-ui/catalog/src/components/catalog/App/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { Modal, BackendModuleDisabled } from '@kyma-project/react-components';
+import { Switch, Route } from 'react-router-dom';
+import { Modal } from '@kyma-project/react-components';
 
-import { NotificationProvider } from '../../../shared/contexts/NotificationContext';
 import ServiceClassList from '../ServiceClassList/ServiceClassList';
 import ServiceClassDetails from '../ServiceClassDetails/ServiceClassDetails';
 import ServiceClassPlansList from '../ServiceClassPlansList/ServiceClassPlansList';
-
-import { backendModuleExists } from '../../../commons/helpers';
 
 Modal.MODAL_APP_REF = '#root';
 
@@ -20,30 +17,22 @@ const ServicePlans = ({ match }) => (
 );
 
 export default function App() {
+  const { path } = useRouteMatch();
+
   return (
-    <>
-      {backendModuleExists('servicecatalog') ? (
-        <NotificationProvider>
-          <BrowserRouter>
-            <Switch>
-              <Route exact path="/" component={ServiceClassList} />
-              <Route exact path="/details/:name" component={ServiceDetails} />
-              <Route
-                exact
-                path="/details/:name/plan/:plan"
-                component={ServiceDetails}
-              />
-              <Route
-                exact
-                path="/details/:name/plans"
-                component={ServicePlans}
-              />
-            </Switch>
-          </BrowserRouter>
-        </NotificationProvider>
-      ) : (
-        <BackendModuleDisabled mod="Service Catalog" />
-      )}
-    </>
+    <Switch>
+      <Route exact path={`${path}/details/:name`} component={ServiceDetails} />
+      <Route
+        exact
+        path={`${path}/details/:name/plan/:plan`}
+        component={ServiceDetails}
+      />
+      <Route
+        exact
+        path={`${path}/details/:name/plans`}
+        component={ServicePlans}
+      />
+      <Route component={ServiceClassList} />
+    </Switch>
   );
 }
