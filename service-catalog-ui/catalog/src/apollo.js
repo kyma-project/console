5ba1b3823bcab1cd58ac527138e31416d9e95e10
Szymon Janota <szymon.janota@sap.com>
2020-04-21 09:58:41 +0200

    Merge apollo clients

--- service-catalog-ui/catalog/src/apollo.js
+++ service-catalog-ui/catalog/src/apollo.js
@@ -8,10 +8,19 @@ import { onError } from 'apollo-link-error';
 import builder from './commons/builder';
 import { getApiUrl as getURL } from '@kyma-project/common';
 
+import { SubscriptionClient } from 'subscriptions-transport-ws';
+
 export function createApolloClient() {
   const graphqlApiUrl = getURL(
     process.env.REACT_APP_LOCAL_API ? 'graphqlApiUrlLocal' : 'graphqlApiUrl',
   );
+
+  const wsLink = new WebSocketLink({
+    uri: getURL('subscriptionsApiUrl'),
+    options: {
+      reconnect: true,
+    },
+  });
 
   const httpLink = createHttpLink({ uri: graphqlApiUrl });
   const authLink = setContext((_, { headers }) => {
@@ -23,6 +32,7 @@ export function createApolloClient() {
     };
   });
   const authHttpLink = authLink.concat(httpLink);
+  const link = split(isSubscriptionOperation, wsLink, httpLink);
 
   const cache = new InMemoryCache();
 
@@ -47,9 +57,38 @@ export function createApolloClient() {
   const client = new ApolloClient({
     uri: graphqlApiUrl,
     cache,
-    link: ApolloLink.from([errorLink, authHttpLink]),
+    link: ApolloLink.from([errorLink, link]),
     connectToDevTools: true,
   });
 
   return client;
+}
+
+class WebSocketLink extends ApolloLink {
+  constructor(paramsOrClient) {
+    super();
+
+    if (paramsOrClient instanceof SubscriptionClient) {
+      this.subscriptionClient = paramsOrClient;
+    } else {
+      const bearerToken = builder.getBearerToken();
+      const protocols = ['graphql-ws'];
+
+      const token = bearerToken ? bearerToken.split(' ')[1] : null;
+      if (token) {
+        protocols.push(token);
+      }
+
+      this.subscriptionClient = new SubscriptionClient(
+        paramsOrClient.uri,
+        paramsOrClient.options,
+        null,
+        protocols,
+      );
+    }
+  }
+
+  request(operation) {
+    return this.subscriptionClient.request(operation);
+  }
 }
