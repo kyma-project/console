a517802d43866cdc73119df23eff297351199e2e
parostatkiem <jan.sudczak@sap.com>
2020-05-07 11:52:43 +0200

    serviceClassList works

--- service-catalog-ui/src/components/App/App.js
+++ service-catalog-ui/src/components/App/App.js
@@ -8,8 +8,9 @@ const App = () => (
   <NotificationProvider>
     <Switch>
       <Route path="/preload" component={() => null} />
-      <Route exact path="/" rende={() => <h1>elo</h1>} />
+      <Route exact path="/" component={ServiceClassList} />
     </Switch>
   </NotificationProvider>
 );
+
 export default App;
