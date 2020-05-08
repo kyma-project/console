b6dfde7bb8714a6d5d8c09d302520f3071986990
parostatkiem <jan.sudczak@sap.com>
2020-05-08 09:25:48 +0200

    add ServicePlanList

--- service-catalog-ui/src/components/App/App.js
+++ service-catalog-ui/src/components/App/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 
 import ServiceClassList from '../ServiceClassList/ServiceClassList';
 import ServiceClassDetails from '../ServiceClassDetails/ServiceClassDetails';
+import ServiceClassPlansList from '../ServiceClassPlansList/ServiceClassPlansList';
 import { NotificationProvider } from 'react-shared';
 
 const App = () => (
@@ -11,12 +12,26 @@ const App = () => (
       <Route path="/preload" component={() => null} />
       <Route exact path="/" component={ServiceClassList} />
       <Route exact path="/details/:name" component={RoutedCatalogDetails} />
+      <Route
+        exact
+        path="/details/:name/plan/:plan"
+        component={RoutedCatalogDetails}
+      />
+      <Route
+        exact
+        path="/details/:name/plans"
+        component={RoutedServicePlanList}
+      />
     </Switch>
   </NotificationProvider>
 );
 
 const RoutedCatalogDetails = ({ match }) => (
   <ServiceClassDetails name={match.params.name} plan={match.params.plan} />
+);
+
+const RoutedServicePlanList = ({ match }) => (
+  <ServiceClassPlansList name={match.params.name} />
 );
 
 export default App;
