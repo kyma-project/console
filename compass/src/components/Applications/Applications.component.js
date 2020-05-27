06cd955547916d00cf479ed56311d2079ce6a39f
Wawrzyn321 <Wawrzyn321@gmail.com>
2020-05-27 09:34:59 +0200

    Unify statuses on compass, update add-ons test

--- compass/src/components/Applications/Applications.component.js
+++ compass/src/components/Applications/Applications.component.js
@@ -14,6 +14,7 @@ import {
 import CreateApplicationModal from './CreateApplicationModal/CreateApplicationModal.container';
 import CreateApplicationFromTemplateModal from './CreateApplicationFromTemplateModal/CreateApplicationFromTemplateModal';
 import ScenariosDisplay from './../Shared/ScenariosDisplay/ScenariosDisplay';
+import { getBadgeTypeForStatus } from './../Shared/getBadgeTypeForStatus';
 
 class Applications extends React.Component {
   static propTypes = {
@@ -47,7 +48,7 @@ class Applications extends React.Component {
     />,
     <Counter>{application.packages.totalCount}</Counter>,
     <StatusBadge
-      autoResolveType
+      type={getBadgeTypeForStatus(application.status)}
       status={
         application.status && application.status.condition
           ? application.status.condition
