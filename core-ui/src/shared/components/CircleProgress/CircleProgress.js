79128f160da4be8463bef045833ad9d2e8ad161a
parostatkiem <jan.sudczak@sap.com>
2020-06-09 15:08:12 +0200

    add inner part

--- core-ui/src/shared/components/CircleProgress/CircleProgress.js
+++ core-ui/src/shared/components/CircleProgress/CircleProgress.js
@@ -17,11 +17,14 @@ const CircleProgress = () => {
         max="100"
         onChange={handleChange}
       ></input>
-      {p} %
+
       <div className="circle-progress">
         <div className="progress-bar">
           <div className={`mask mask--dynamic fill--${p}`}></div>
           <div className={`mask mask--permanent`}></div>
+        </div>
+        <div className="inner-area">
+          <div className="percentage">{p}%</div>
         </div>
       </div>
     </>
