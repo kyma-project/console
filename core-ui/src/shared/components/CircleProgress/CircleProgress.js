60f3e10d8b1789680f3fa5207bbc454107184226
parostatkiem <jan.sudczak@sap.com>
2020-06-10 09:04:27 +0200

    cleanup

--- core-ui/src/shared/components/CircleProgress/CircleProgress.js
+++ core-ui/src/shared/components/CircleProgress/CircleProgress.js
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
-import './CircleProgress.scss';
+import React from 'react';
 import PropTypes from 'prop-types';
+import './CircleProgress.scss';
+
 const CircleProgress = ({ value, color = 'blue', children }) => {
   return (
     <div className="circle-progress">
       <div className={`circle--${color}`}>
         <div className="progress-bar">
-          <div className={`mask mask--dynamic fill--${value}`}></div>
-          <div className={`mask mask--permanent`}></div>
+          <div className={`mask--dynamic fill--${value}`}></div>
+          <div className={`mask--permanent`}></div>
         </div>
         <div className="inner-area">
           <div className="percentage">{value}%</div>
@@ -20,14 +21,14 @@ const CircleProgress = ({ value, color = 'blue', children }) => {
 
 CircleProgress.propTypes = {
   color: PropTypes.oneOf(['purple', 'green', 'blue', 'teal']),
-  value: function(props, propName) {
+  value: (props, propName) => {
     if (
       !Number.isInteger(props[propName]) ||
       props[propName] < 0 ||
       props[propName] > 100
     ) {
       return new Error(
-        'Value property is required and must be an integer of range [0,100]',
+        `'value' property is required and must be an integer of range [0,100]`,
       );
     }
   },
