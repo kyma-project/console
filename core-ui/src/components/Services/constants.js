08620f4a8633d2baa12766b6ab681115f6aa9825
parostatkiem <jan.sudczak@sap.com>
2020-07-08 15:14:09 +0200

    Update another constant

--- core-ui/src/components/Services/constants.js
+++ core-ui/src/components/Services/constants.js
@@ -1,6 +1,7 @@
 export const EVENT_TRIGGERS = {
   TYPE: 'Service',
-  NOT_FOUND_MESSAGE: 'This service has no event triggers',
+  NOT_FOUND_MESSAGE:
+    'There are no Event Triggers connected to this Service yet.',
 };
 
 export const SERVICE_API_VERSION = 'v1';
