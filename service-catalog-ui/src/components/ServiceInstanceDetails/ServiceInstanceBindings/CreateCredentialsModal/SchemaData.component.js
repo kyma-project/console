d819d773d6ef43ad1e26bda940c8c219982bb0ac
Piotr Wawrzyńczyk <Wawrzyn321@gmail.com>
2020-08-11 21:49:04 +0200

    Restore SchemaData.component.js

--- service-catalog-ui/src/components/ServiceInstanceDetails/ServiceInstanceBindings/CreateCredentialsModal/SchemaData.component.js
+++ service-catalog-ui/src/components/ServiceInstanceDetails/ServiceInstanceBindings/CreateCredentialsModal/SchemaData.component.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// import {
-//   JsonSchemaForm,
-//   Icon,
-//   ErrorBoundary,
-// } from '@kyma-project/react-components';
+import {
+  JsonSchemaForm,
+  Icon,
+  ErrorBoundary,
+} from '@kyma-project/react-components';
 import { Bold } from './styled';
 
 class SchemaData extends React.Component {
@@ -36,35 +36,34 @@ class SchemaData extends React.Component {
   };
 
   render() {
-    return <Bold>?</Bold>;
-    // const {
-    //   bindingCreateParameterSchema,
-    //   onSubmitSchemaForm,
-    //   children,
-    //   planName,
-    // } = this.props;
-    // const { bindingCreateParameters } = this.state;
+    const {
+      bindingCreateParameterSchema,
+      onSubmitSchemaForm,
+      children,
+      planName,
+    } = this.props;
+    const { bindingCreateParameters } = this.state;
 
-    // return (
-    //   <ErrorBoundary
-    //     content={
-    //       <div>
-    //         <Icon glyph="error" style={{ padding: '0 5px 0 0' }} /> Incorrect
-    //         Binding Create Parameter schema in <Bold>{planName}</Bold> plan
-    //       </div>
-    //     }
-    //   >
-    //     <JsonSchemaForm
-    //       schema={bindingCreateParameterSchema}
-    //       onChange={this.onChangeSchemaForm}
-    //       liveValidate={true}
-    //       onSubmit={onSubmitSchemaForm}
-    //       formData={bindingCreateParameters}
-    //     >
-    //       {children}
-    //     </JsonSchemaForm>
-    //   </ErrorBoundary>
-    // );
+    return (
+      <ErrorBoundary
+        content={
+          <div>
+            <Icon glyph="error" style={{ padding: '0 5px 0 0' }} /> Incorrect
+            Binding Create Parameter schema in <Bold>{planName}</Bold> plan
+          </div>
+        }
+      >
+        <JsonSchemaForm
+          schema={bindingCreateParameterSchema}
+          onChange={this.onChangeSchemaForm}
+          liveValidate={true}
+          onSubmit={onSubmitSchemaForm}
+          formData={bindingCreateParameters}
+        >
+          {children}
+        </JsonSchemaForm>
+      </ErrorBoundary>
+    );
   }
 }
 
