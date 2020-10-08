cdcb2f4a16ad4cb0f19c180c646647144589fa0a
dariadomagala <daria.domagala@sap.com>
2020-10-08 14:17:42 +0200

    Add envs

--- core/karma.conf.js
+++ core/karma.conf.js
@@ -26,10 +26,10 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromiumHeadlessWithFlag'],
+    browsers: ['ChromeHeadlessWithFlag'],
     customLaunchers: {
-      ChromiumHeadlessWithFlag: {
-        base: 'ChromiumHeadless',
+      ChromeHeadlessWithFlag: {
+        base: 'ChromeHeadless',
         flags: ['--no-sandbox']
       }
     },
