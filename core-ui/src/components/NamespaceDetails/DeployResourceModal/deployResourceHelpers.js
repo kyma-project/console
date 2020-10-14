400229ac2b987b758e719cffe5b2a3cd3a8ee4e5
Wawrzyn321 <Wawrzyn321@gmail.com>
2020-10-14 20:43:56 +0200

    Check for name instead of type, adjust tests

--- core-ui/src/components/NamespaceDetails/DeployResourceModal/deployResourceHelpers.js
+++ core-ui/src/components/NamespaceDetails/DeployResourceModal/deployResourceHelpers.js
@@ -8,10 +8,9 @@ function validateFile(file) {
   if (!file.size) {
     return 'File cannot be empty';
   }
-  console.log(file);
-  console.log(file.type);
-  const allowedTypes = ['application/json', 'application/x-yaml'];
-  if (!allowedTypes.includes(file.type)) {
+
+  const allowedExtensionRegex = /\.(ya?ml|json)$/;
+  if (!allowedExtensionRegex.test(file.name)) {
     return 'Invalid file extension';
   }
   return '';
