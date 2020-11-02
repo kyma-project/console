a349e2e01ceb08ebbce836eb114c95c473a2ce76
Agata Kucharska <akucharska@users.noreply.github.com>
2020-11-02 11:03:04 +0100

    Update core-ui/src/components/Secrets/Secrets.js
    Co-authored-by: Piotr Wawrzyńczyk <Wawrzyn321@gmail.com>

--- core-ui/src/components/Secrets/Secrets.js
+++ core-ui/src/components/Secrets/Secrets.js
@@ -5,9 +5,9 @@ import { PageHeader, YamlEditorProvider } from 'react-shared';
 import SecretList from './List/SecretList';
 import { SECRETS_TITLE } from 'shared/constants';
 
-Services.propTypes = { namespace: PropTypes.string.isRequired };
+Secrets.propTypes = { namespace: PropTypes.string.isRequired };
 
-export default function Services({ namespace }) {
+export default function Secrets({ namespace }) {
   return (
     <YamlEditorProvider>
       <PageHeader title={SECRETS_TITLE} />
