6b2fd4b078034da3ac83d15fa914a717d3a5a20b
parostatkiem <jan.sudczak@sap.com>
2020-12-02 15:26:46 +0100

    fix subscriptions - ADDED type event not coming

--- core-ui/src/components/Pods/PodList/PodList.js
+++ core-ui/src/components/Pods/PodList/PodList.js
@@ -14,40 +14,11 @@ import {
   useUpdate,
   useDelete,
   useSubscription,
+  handleSubscriptionEvent,
 } from 'react-shared';
 import Moment from 'react-moment';
 
 PodList.propTypes = { namespace: PropTypes.string.isRequired };
-
-export function handleSubscriptionEvent(setResource) {
-  const filterByName = obj => entry =>
-    entry.metadata.name !== obj.metadata.name;
-
-  return data => {
-    const { type, object } = data;
-    switch (type) {
-      case 'ADDED':
-        setResource(resource => {
-          if (!resource.find(r => r.metadata.name === object.metadata.name)) {
-            return [...resource, object];
-          }
-          return resource;
-        });
-        break;
-      case 'DELETED':
-        setResource(resource => resource.filter(filterByName(object)));
-        break;
-      case 'MODIFIED':
-        setResource(
-          resource => resource.map(r => (filterByName(object)(r) ? r : object)), // fancy
-        );
-        break;
-      default:
-        console.warn(data);
-        break;
-    }
-  };
-}
 
 const PodStatus = ({ pod }) => {
   const phase = pod.status?.phase; //TODO consider statusConditions to calculate status
--- shared/hooks/ScalpAPI/useSubscription.js
+++ shared/hooks/ScalpAPI/useSubscription.js
@@ -39,3 +39,39 @@ export const useSubscription = (
 todo 
 const {resource, loading, error } = useSubscribedResource(resourceType, resourceFilter?);
 */
+
+export function handleSubscriptionEvent(setResource) {
+  const filterByName = obj => entry =>
+    entry.metadata.name !== obj.metadata.name;
+
+  return data => {
+    const { type, object } = data;
+    switch (type) {
+      case 'ADDED':
+        setResource(resource => {
+          if (!resource.find(r => r.metadata.name === object.metadata.name)) {
+            return [...resource, object];
+          }
+          return resource;
+        });
+        break;
+      case 'DELETED':
+        setResource(resource => resource.filter(filterByName(object)));
+        break;
+      case 'MODIFIED':
+        setResource(
+          resource => {
+            if (!resource.find(r => r.metadata.name === object.metadata.name)) {
+              return [...resource, object];
+            }
+
+            return resource.map(r => (filterByName(object)(r) ? r : object));
+          }, // fancy
+        );
+        break;
+      default:
+        console.warn(data);
+        break;
+    }
+  };
+}
