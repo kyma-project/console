0e8c382d86caace79c6f73c56cb2032e82280ddb
Wawrzyn321 <Wawrzyn321@gmail.com>
2020-12-23 13:28:44 +0100

    Fix hook

--- core-ui/src/components/NamespaceDetails/CreateWorkloadForm/ScalingData.js
+++ core-ui/src/components/NamespaceDetails/CreateWorkloadForm/ScalingData.js
@@ -55,6 +55,7 @@ export default function ScalingData({ deployment, setDeployment }) {
         },
       });
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [preset]);
 
   return (
