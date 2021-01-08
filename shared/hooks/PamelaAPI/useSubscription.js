af9fa70e062b152dee24c85515d75792146b7187
parostatkiem <jan.sudczak@sap.com>
2021-01-08 12:02:20 +0100

    disable subscriptions

--- shared/hooks/PamelaAPI/useSubscription.js
+++ shared/hooks/PamelaAPI/useSubscription.js
@@ -14,25 +14,23 @@ export const useSubscription = (
   let url = baseUrl(fromConfig);
 
   React.useEffect(() => {
-    if (!idToken) return;
-    const socket = io(url, {
-      query: {
-        ...urlTemplateVariables,
-        resource,
-        authorization: 'Bearer ' + idToken,
-      },
-      transports: ['websocket', 'polling'],
-    });
-
-    // socket.on('connect_error', err => console.log('CONNECT_ERROR', err));
-    // socket.on('connect_timeout', err => console.log('CONNECT_TIMEOUT', err));
-    // socket.on('connect', () => console.log('CONNECT'));
-    // socket.on('disconnect', () => console.log('DISCONNECT'));
-    // socket.on('error', err => console.log('DISCONNECT', err));
-    // socket.on('reconnect', err => console.log('RECONNECT', err));
-
-    socket.on('message', dispatch);
-    return () => socket.disconnect();
+    // if (!idToken) return;
+    // const socket = io(url, {
+    //   query: {
+    //     ...urlTemplateVariables,
+    //     resource,
+    //     authorization: 'Bearer ' + idToken,
+    //   },
+    //   transports: ['websocket', 'polling'],
+    // });
+    // // socket.on('connect_error', err => console.log('CONNECT_ERROR', err));
+    // // socket.on('connect_timeout', err => console.log('CONNECT_TIMEOUT', err));
+    // // socket.on('connect', () => console.log('CONNECT'));
+    // // socket.on('disconnect', () => console.log('DISCONNECT'));
+    // // socket.on('error', err => console.log('DISCONNECT', err));
+    // // socket.on('reconnect', err => console.log('RECONNECT', err));
+    // socket.on('message', dispatch);
+    // return () => socket.disconnect();
   }, [resource, dispatch, idToken]);
 };
 
