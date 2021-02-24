9f253fffc89f588ca96f596edf237e508cee97b8
Wawrzyn321 <Wawrzyn321@gmail.com>
2021-02-24 13:44:48 +0100

    Remove commented code

--- core-ui/src/components/Predefined/List/Application/Applications.list.js
+++ core-ui/src/components/Predefined/List/Application/Applications.list.js
@@ -2,22 +2,9 @@ import React from 'react';
 import CreateApplicationModal from './CreateApplicationModal';
 
 export const ApplicationsList = DefaultRenderer => ({ ...otherParams }) => {
-  const customHeaderActions = <CreateApplicationModal />;
-  //   const customColumns = [
-  //     {
-  //       header: 'Connected Namespaces',
-  //       value: app => {
-  //         console.log(app);
-  //         return (
-  //           <p>ok</p>
-  //         );
-  //       },
-  //     },
-  //   ];
   return (
     <DefaultRenderer
-      customHeaderActions={customHeaderActions}
-      //   customColumns={customColumns}
+      customHeaderActions={<CreateApplicationModal />}
       {...otherParams}
     />
   );
