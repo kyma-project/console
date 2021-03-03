e62552a7aaaadde2f483f7af21a66ef1dbddc3c7
Wawrzyn321 <Wawrzyn321@gmail.com>
2021-03-03 13:43:53 +0100

    Create for OAuth clients

---
+++ core-ui/src/components/Predefined/Create/OAuthClients/OAuthClients.create.js
@@ -0,0 +1,125 @@
+import React from 'react';
+
+import { useGetList, usePost, StringInput, K8sNameInput } from 'react-shared';
+
+import { FormItem, FormLabel } from 'fundamental-react';
+import CheckboxFormControl from './CheckboxFormControl';
+import { grantTypes, responseTypes } from 'components/OAuthClients/common';
+import { createOAuthClient } from './createOAuthClient';
+
+function validateSpec(spec) {
+  const { grantTypes, responseTypes, scope } = spec;
+  return grantTypes.length >= 1 && responseTypes.length >= 1 && !!scope;
+}
+
+const emptySpec = {
+  name: '',
+  scope: '',
+  responseTypes: [],
+  grantTypes: [],
+  secretName: '',
+};
+
+export const OAuth2ClientsCreate = ({
+  namespace,
+  formElementRef,
+  onChange,
+  onCompleted,
+  onError,
+  resourceUrl,
+  refetchList,
+  setCustomValid,
+}) => {
+  const postRequest = usePost();
+  const [spec, setSpec] = React.useState(emptySpec);
+  const [useCustomSecret, setUseCustomSecret] = React.useState(false);
+
+  const { data } = useGetList()(`/api/v1/namespaces/${namespace}/secrets/`, {
+    pollingInterval: 3000,
+  });
+  const secretNames = data?.map(s => s.metadata.name) || [];
+
+  React.useEffect(() => setCustomValid(validateSpec(spec)), [
+    spec,
+    useCustomSecret,
+  ]);
+
+  async function handleFormSubmit(e) {
+    e.preventDefault();
+    if (!useCustomSecret) {
+      spec.secretName = spec.name;
+    }
+    const input = createOAuthClient(namespace, spec);
+    try {
+      await postRequest(resourceUrl, input);
+      refetchList();
+      onCompleted(spec.name, `Client created`);
+    } catch (e) {
+      console.warn(e);
+      onError(`The client could not be created:`, e.message);
+    }
+  }
+
+  return (
+    <form ref={formElementRef} onChange={onChange} onSubmit={handleFormSubmit}>
+      <FormItem>
+        <K8sNameInput
+          value={spec.name}
+          label="Name"
+          kind="Client"
+          onChange={e => setSpec({ ...spec, name: e.target.value })}
+        />
+      </FormItem>
+      <FormItem className="clearfix">
+        <CheckboxFormControl
+          name="Response types"
+          availableValues={responseTypes}
+          values={spec.responseTypes}
+          onChange={values => setSpec({ ...spec, responseTypes: values })}
+        />
+      </FormItem>
+      <FormItem className="clearfix">
+        <CheckboxFormControl
+          name="Grant types"
+          availableValues={grantTypes}
+          values={spec.grantTypes}
+          onChange={values => setSpec({ ...spec, grantTypes: values })}
+        />
+      </FormItem>
+      <FormItem>
+        <FormLabel htmlFor="scope" required className="fd-has-display-block">
+          Scope
+        </FormLabel>
+        <StringInput
+          stringList={spec.scope.split(' ').filter(scope => scope)}
+          onChange={scope => setSpec({ ...spec, scope: scope.join(' ') })}
+          id="scope"
+        />
+      </FormItem>
+      <p
+        className="link fd-has-display-block fd-has-margin-top-small"
+        onClick={() => setUseCustomSecret(!useCustomSecret)}
+      >
+        {useCustomSecret
+          ? 'Create secret with the same name as client.'
+          : 'Define custom secret name for this client.'}
+      </p>
+      {useCustomSecret && (
+        <>
+          <FormItem>
+            <K8sNameInput
+              label="Secret name"
+              kind="Secret"
+              onChange={e => setSpec({ ...spec, secretName: e.target.value })}
+              value={spec.secretName}
+            />
+          </FormItem>
+          <p className="fd-has-display-block fd-has-color-text-3">
+            {secretNames?.includes(spec.secretName) &&
+              `Secret "${spec.secretName}" exists and will be used for this client.`}
+          </p>
+        </>
+      )}
+    </form>
+  );
+};
