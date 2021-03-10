bea4c00209ac30e447be9ed327cc295504119556
Wawrzyn321 <Wawrzyn321@gmail.com>
2021-03-10 13:53:44 +0100

    Adjust shared

--- shared/utils/getClusterConfig.js
+++ shared/utils/getClusterConfig.js
@@ -1,9 +1,21 @@
 const domain = location.hostname.replace(/^console(-dev)?\./, '');
+const isNpx = location.origin === 'http://localhost:3001';
 const isLocalDev = location.hostname.startsWith('console-dev');
+
+function createPamelaApiUrl() {
+  switch (true) {
+    case isNpx:
+      return 'http://localhost:3001/backend';
+    case isLocalDev:
+      return 'http://localhost:3001';
+    default:
+      return 'https://' + domain + '/backend';
+  }
+}
 
 export const getClusterConfig = () => ({
   domain,
   graphqlApiUrl: `https://console-backend.${domain}/graphql`,
   subscriptionsApiUrl: `wss://console-backend.${domain}/graphql`,
-  pamelaApiUrl: 'http://localhost:3001/backend',
+  pamelaApiUrl: createPamelaApiUrl(),
 });
