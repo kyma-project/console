2b4df8ce821dd805ec2c69a2c84007d8c7ee0298
rJankowski93 <rafal.jankowski.93@gmail.com>
2021-04-16 12:17:13 +0200

    Fix notify

--- core-ui/src/components/Lambdas/LambdaDetails/LambdaDetailsWrapper.js
+++ core-ui/src/components/Lambdas/LambdaDetails/LambdaDetailsWrapper.js
@@ -4,12 +4,11 @@ import LuigiClient from '@luigi-project/client';
 import { useLambdaQuery } from 'components/Lambdas/gql/hooks/queries';
 
 import EntryNotFound from '../../EntryNotFound/EntryNotFound';
-import { Spinner } from 'react-shared';
+import { Spinner, useNotification } from 'react-shared';
 
 import LambdaDetails from './LambdaDetails';
 
 import './LambdaDetails.scss';
-import { useNotification } from '../../../../../shared';
 
 export default function LambdaDetailsWrapper({ lambdaName }) {
   const { lambda, error, loading } = useLambdaQuery({
